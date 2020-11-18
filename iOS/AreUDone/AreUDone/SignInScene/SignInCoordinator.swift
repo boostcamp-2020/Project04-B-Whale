f209814958458e8c9d15bfd9c3589557046dcecc
Seo Myung Ryul <smr3102@naver.com>
2020-11-18 21:26:27 +0900

    [FEAT] SignInCoordinator 수정
    - SignInCoordinator 코드 추가

--- iOS/AreUDone/AreUDone/SignInScene/SignInCoordinator.swift
+++ iOS/AreUDone/AreUDone/SignInScene/SignInCoordinator.swift
@@ -5,10 +5,20 @@
 //  Created by 서명렬 on 2020/11/18.
 //
 
-import Foundation
+import UIKit
 
 final class SignInCoordinator: Coordinator {
+  
+  private var storyboard: UIStoryboard {
+    return UIStoryboard.storyboard(storyboard: .signIn)
+  }
+  
   func start() -> UIViewController {
+    // TODO:- SignInViewController의 init 수정 시 같이 수정해야함.
+    guard let signInViewController = storyboard.instantiateInitialViewController() else {
+      return UIViewController()
+    }
     
+    return signInViewController
   }
 }
