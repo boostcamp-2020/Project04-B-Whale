941648a908094db6461a6b646133969216761e5f
tksrl0379 <tksrl0379@naver.com>
2020-11-18 22:53:46 +0900

    [FEAT] SignInViewController 생성자 생성
    - SignInViewController 생성자 생성

--- iOS/AreUDone/AreUDone/SignInScene/View/Controller/SignInViewController.swift
+++ iOS/AreUDone/AreUDone/SignInScene/View/Controller/SignInViewController.swift
@@ -9,6 +9,18 @@ import UIKit
 
 class SignInViewController: UIViewController {
   
+  private let viewModel: SignInViewModelProtocol
+  
+  init?(coder: NSCoder, viewModel: SignInViewModelProtocol) {
+    self.viewModel = viewModel
+    
+    super.init(coder: coder)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("This controller must be initialized with code")
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
