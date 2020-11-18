f9959cc453d258fe07c00996c0d6c2cf3cd972cc
Seo Myung Ryul <smr3102@naver.com>
2020-11-18 21:15:15 +0900

    [FEAT] AppCoordinator 생성
    - Coordinator를 채택한 AppCoordinator 생성

---
+++ iOS/AreUDone/AreUDone/LoginScene/SignInCoordinator.swift
@@ -0,0 +1,14 @@
+//
+//  SignInCoordinator.swift
+//  AreUDone
+//
+//  Created by 서명렬 on 2020/11/18.
+//
+
+import Foundation
+
+final class SignInCoordinator: Coordinator {
+  func start() -> UIViewController {
+    
+  }
+}
