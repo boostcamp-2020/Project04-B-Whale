c8170715d5fc03dff191411b039715b6103cbbd6
Seo Myung Ryul <smr3102@naver.com>
2020-11-19 15:02:00 +0900

    [FEAT] ServiceLayer 추가
    - UserService 추가
    - CardService 추가
    - BoardService 추가
    - ListService 추가
    - CommentService 추가
    - ActivityService 추가

---
+++ iOS/AreUDone/AreUDone/Common/Model/Service/CardService.swift
@@ -0,0 +1,8 @@
+//
+//  CardService.swift
+//  AreUDone
+//
+//  Created by 서명렬 on 2020/11/19.
+//
+
+import Foundation
---
+++ iOS/AreUDone/AreUDone/Common/Model/Service/UserService.swift
@@ -0,0 +1,8 @@
+//
+//  UserService.swift
+//  AreUDone
+//
+//  Created by 서명렬 on 2020/11/19.
+//
+
+import Foundation
