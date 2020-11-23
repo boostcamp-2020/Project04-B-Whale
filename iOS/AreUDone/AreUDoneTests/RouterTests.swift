13bcc774f9e8bdfcc6c80091b6778c0d05293211
tksrl0379 <tksrl0379@naver.com>
2020-11-23 15:59:48 +0900

    [TEST] Mock Router 테스트 코드 추가
    - Mock Router 테스트 코드 추가

---
+++ iOS/AreUDone/AreUDoneTests/RouterTests.swift
@@ -0,0 +1,42 @@
+//
+//  RouterTests.swift
+//  AreUDoneTests
+//
+//  Created by a1111 on 2020/11/23.
+//
+
+import XCTest
+
+
+class RouterTests: XCTestCase {
+  
+  override func setUpWithError() throws {
+  }
+  
+  override func tearDownWithError() throws {
+  }
+  
+  func testMockRouter_유효데이터() {
+    CardService(router: MockRouter(jsonFactory: CardTrueJsonFactory())).fetchDailyCards(date: "") { result in
+      switch result {
+      case .success:
+        XCTAssert(true)
+      case .failure:
+        XCTAssert(false)
+      }
+    }
+  }
+  
+  func testMockRouter_비유효데이터() {
+    CardService(router: MockRouter(jsonFactory: CardFalseJsonFactory())).fetchDailyCards(date: "") { result in
+      switch result {
+      case .success:
+        XCTAssert(false)
+      case .failure:
+        XCTAssert(true)
+      }
+    }
+  }
+  
+  
+}
