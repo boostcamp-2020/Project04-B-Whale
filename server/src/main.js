715e77a8362caad75938e70c560b74df13424776
youngxpepp <youngxpepp@gmail.com>
2020-11-24 13:47:02 +0900

    [FIX] ApplicationFactory 제거
    - 팩토리 패턴을 사용할 만큼 Application을 생성하는 로직이 복잡하지 않다
    - main에서 직접 초기화를 해주는 것이 직관적일 수 있다

--- server/src/main.js
+++ server/src/main.js
@@ -1,10 +1,10 @@
 import { Application } from './Application';
-import { ApplicationFactory } from './ApplicationFactory';
 
 async function main() {
-    const app = await ApplicationFactory.create();
-    await app.listen(process.env.PORT || 3000);
-    console.log(`Server listening on ${process.env.PORT || 3000}`);
+    const app = new Application();
+    await app.initialize();
+    await app.listen(process.env.PORT || 5000);
+    console.log(`Server listening on ${process.env.PORT || 5000}`);
 }
 
 main();
