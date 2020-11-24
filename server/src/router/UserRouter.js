b2ebae711ddc0d5f37810921cf01d1e8e2424368
youngxpepp <youngxpepp@gmail.com>
2020-11-24 18:19:53 +0900

    [FIX] passport 미들웨어를 상위 라우터에 적용
    - 각 API에 passport를 적용하는 것은 불필요한 작업
    - 모든 서비스 API가 로그인이 필요한 작업이기 때문에 상위 라우터에 옮김

--- server/src/router/UserRouter.js
+++ server/src/router/UserRouter.js
@@ -1,11 +1,10 @@
 import { Router } from 'express';
-import passport from 'passport';
 import { UserService } from '../service/UserService';
 
 export const UserRouter = () => {
     const router = Router();
 
-    router.get('/me', passport.authenticate('jwt', { session: false }), async (req, res) => {
+    router.get('/me', async (req, res) => {
         const userService = UserService.getInstance();
         const user = await userService.getUserById(req?.user?.id);
         res.status(200).json({
--- server/src/router/index.js
+++ server/src/router/index.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import passport from 'passport';
 import { OAuthRouter } from './OAuthRouter';
 import { UserRouter } from './UserRouter';
 
@@ -6,7 +7,7 @@ export const IndexRouter = () => {
     const router = Router();
 
     router.use('/api/oauth', OAuthRouter());
-    router.use('/api/user', UserRouter());
+    router.use('/api/user', passport.authenticate('jwt', { session: false }), UserRouter());
 
     return router;
 };
