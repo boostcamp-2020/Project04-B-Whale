fabe1bcb978b0b95afdfd7ec546ae8b671cea1ae
Sooyeon Park <49746644+Park-SooYeon@users.noreply.github.com>
2020-11-25 15:07:44 +0900

    Merge branch 'feature/get-boards-api' into 'develope/web'
    * [FEAT] Board 조회에 사용할 service 함수 추가
    
    - 내가 가진 보드만 조회하는 getMyBoards 함수 추가
    - 초대 받은 보드만 조회하는 getInvitedBoards 함수 추가
    
    * [FEAT] board api 사용을 위한 라우터 설정 추가
    
    - board api의 요청 경로를 설정하는 board router 추가
    
    * [FEAT] board router에 jwt 권한 확인 추가
    
    - board router에 jwt 권한 확인 추가
    
    * [FIX] 응답 결과가 api 명세와 불일치하는 문제 수정
    
    - jwt에 저장된 user id를 사용하여 board 조회하도록 변경
    - 응답 결과가 api 명세와 불일치하는 문제 수정
    
    * [FIX] 리뷰 피드백 반영
    
    - json 응답 코드 변경
    - 초대 받은 보드를 불러올 때 사용하는 inner join문 컬럼명 수정
    - 보드 전체 조회 로직을 비동기적으로 변경
    
    * [TEST] GET /api/board API 테스트 코드 추가
    
    - GET /api/board API 테스트 코드 추가

---
+++ server/src/router/BoardRouter.js
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import { BoardService } from '../service/BoardService';
+
+export const BoardRouter = () => {
+    const router = Router();
+
+    router.get('/', async (req, res) => {
+        const boardService = BoardService.getInstance();
+        const { id } = req.user;
+        const data = await boardService.getBoardsByUserId(id);
+        res.status(200).json(data);
+    });
+
+    return router;
+};
--- server/src/service/BoardService.js
+++ server/src/service/BoardService.js
@@ -1,3 +1,4 @@
+import { Transactional } from 'typeorm-transactional-cls-hooked';
 import { BaseService } from './BaseService';
 
 export class BoardService extends BaseService {
@@ -9,5 +10,33 @@ export class BoardService extends BaseService {
         }
 
         return BoardService.instance;
+    }
+
+    async getMyBoards(id) {
+        const boards = await this.boardRepository.find({
+            select: ['id', 'title'],
+            where: { creator: id },
+        });
+        return boards;
+    }
+
+    async getInvitedBoards(id) {
+        const boards = await this.boardRepository
+            .createQueryBuilder('board')
+            .select('board.id')
+            .addSelect('board.title')
+            .innerJoin('board.invitations', 'invitation', 'invitation.user_id=:userId', {
+                userId: id,
+            })
+            .getMany();
+        return boards;
+    }
+
+    @Transactional()
+    async getBoardsByUserId(userId) {
+        const promises = [this.getMyBoards(userId), this.getInvitedBoards(userId)];
+        const [myBoards, invitedBoards] = await Promise.all(promises);
+
+        return { myBoards, invitedBoards };
     }
 }
