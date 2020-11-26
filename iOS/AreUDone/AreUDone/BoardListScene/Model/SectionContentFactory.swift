4bd5b7a8cc09b878fb603d4e26978cdde9f30a2c
tksrl0379 <tksrl0379@naver.com>
2020-11-26 14:05:42 +0900

    [FEAT] Section 헤더를 위한 Factory 메서드 추가
    - Section 헤더를 위한 Factory 메서드 추가

---
+++ iOS/AreUDone/AreUDone/BoardListScene/Model/SectionContentFactory.swift
@@ -0,0 +1,26 @@
+//
+//  SectionContentFactory.swift
+//  AreUDone
+//
+//  Created by a1111 on 2020/11/26.
+//
+
+import Foundation
+
+protocol SectionContentsFactoryable {
+  func load(index: Int) -> String
+}
+
+struct SectionContentsFactory: SectionContentsFactoryable {
+  
+  func load(index: Int) -> String {
+    switch index {
+    case 0:
+      return "나의 보드"
+    case 1:
+      return "초대된 보드"
+    default:
+      return ""
+    }
+  }
+}
