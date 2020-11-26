d425b5f61dfcc9fd3b8328b1fec87892605ec642
Seo Myung Ryul <smr3102@naver.com>
2020-11-26 12:12:27 +0900

    [FEAT] viewmodel 바인딩
    - viewmodel 바인딩
    - fetchDetailData 구현 (현재는 fake 데이터로 구현)

--- iOS/AreUDone/AreUDone/DetailCardScene/View/Controller/DetailCardViewController.swift
+++ iOS/AreUDone/AreUDone/DetailCardScene/View/Controller/DetailCardViewController.swift
@@ -13,6 +13,29 @@ final class DetailCardViewController: UIViewController {
   
   private let viewModel: DetailCardViewModelProtocol
   private var observer: NSKeyValueObservation?
+  
+  private lazy var scrollView: UIScrollView = {
+    let scrollView = UIScrollView()
+    scrollView.showsVerticalScrollIndicator = false
+    scrollView.showsHorizontalScrollIndicator = false
+    scrollView.backgroundColor = .red
+    
+    return scrollView
+  }()
+  
+  private lazy var stackView: DetailCardStackView = {
+    let stackView = DetailCardStackView()
+    
+    return stackView
+  }()
+  
+  private lazy var detailCardTableView: UITableView = {
+    let tableView = UITableView(frame: CGRect.zero, style: .grouped)
+    
+    return tableView
+  }()
+  
+  
   // MARK:- Initializer
   
   init?(coder: NSCoder, viewModel: DetailCardViewModelProtocol) {
@@ -30,19 +53,80 @@ final class DetailCardViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    
     configure()
     bindUI()
+    
+    viewModel.fetchDetailCard()
   }
+}
+
+
+// MARK:- Extension
+
+private extension DetailCardViewController {
   
-  private func configure() {
+  // TODO:- make datasource
+}
+
+
+// MARK:- Extension Configure Method
+
+private extension DetailCardViewController {
+  
+  func configure() {
     navigationController?.navigationBar.isHidden = false
     navigationController?.navigationBar.prefersLargeTitles = true
+    
+    view.addSubview(scrollView)
+    
+    configureScrollView()
+    configureStackView()
   }
+  
+  func configureScrollView() {
+    scrollView.translatesAutoresizingMaskIntoConstraints = false
+    
+    NSLayoutConstraint.activate([
+      scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+      scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+      scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+      scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+    ])
+  }
+  
+  func configureStackView() {
+    scrollView.addSubview(stackView)
+    stackView.translatesAutoresizingMaskIntoConstraints = false
+    
+    NSLayoutConstraint.activate([
+      stackView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+      stackView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+      stackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+      stackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+      stackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor)
+    ])
+  }
+  
+  func configureDetailCardTableView() {
+    stackView.addArrangedSubview(detailCardTableView)
+    detailCardTableView.translatesAutoresizingMaskIntoConstraints = false
+    
+    NSLayoutConstraint.activate([
+      detailCardTableView.widthAnchor.constraint(equalTo: stackView.widthAnchor)
+    ])
+  }
+}
+
+
+// MARK:- Extension BindUI
+
+private extension DetailCardViewController {
   
   private func bindUI() {
     observationNavigationBar()
-    bindingDetailCardView()
+    bindingDetailCardContentView()
+    bindingDetailCardDueDateView()
+    bindingDetailCardCommentsView()
   }
   
   private func observationNavigationBar() {
@@ -60,7 +144,21 @@ final class DetailCardViewController: UIViewController {
       })
   }
   
-  private func bindingDetailCardView() {
-    
+  private func bindingDetailCardContentView() {
+    viewModel.bindingDetailCardContentView { [weak self] content in
+      self?.stackView.updateContentView(with: content)
+    }
+  }
+  
+  private func bindingDetailCardDueDateView() {
+    viewModel.bindingDetailCardDueDateView { [weak self] dueDate in
+      self?.stackView.updateDueDateView(with: dueDate)
+    }
+  }
+  
+  private func bindingDetailCardCommentsView() {
+    viewModel.bindingDetailCardCommentsView { comments in
+      
+    }
   }
 }
--- iOS/AreUDone/AreUDone/DetailCardScene/ViewModel/DetailCardViewModel.swift
+++ iOS/AreUDone/AreUDone/DetailCardScene/ViewModel/DetailCardViewModel.swift
@@ -8,7 +8,11 @@
 import Foundation
 
 protocol DetailCardViewModelProtocol {
-  func bindingDetailCardView(handler: @escaping (() -> Void))
+  func bindingDetailCardContentView(handler: @escaping ((String) -> Void))
+  func bindingDetailCardDueDateView(handler: @escaping ((String) -> Void))
+  func bindingDetailCardCommentsView(handler: @escaping (([DetailCard.Comment]) -> Void))
+  
+  func fetchDetailCard()
 }
 
 final class DetailCardViewModel: DetailCardViewModelProtocol {
@@ -18,6 +22,9 @@ final class DetailCardViewModel: DetailCardViewModelProtocol {
   private var cardService: CardServiceProtocol
   private let id: Int
   
+  private var detailCardContentViewHandler: ((String) -> Void)?
+  private var detailCardDueDateViewHandler: ((String) -> Void)?
+  private var detailCardCommentsViewHandler: (([DetailCard.Comment]) -> Void)?
   
   // MARK:- Initializer
   
@@ -27,7 +34,34 @@ final class DetailCardViewModel: DetailCardViewModelProtocol {
   }
   
   
-  func bindingDetailCardView(handler: @escaping (() -> Void)) {
+  func bindingDetailCardContentView(handler: @escaping ((String) -> Void)) {
+    detailCardContentViewHandler = handler
+  }
+  
+  func bindingDetailCardDueDateView(handler: @escaping ((String) -> Void)) {
+    detailCardDueDateViewHandler = handler
+  }
+  
+  func bindingDetailCardCommentsView(handler: @escaping (([DetailCard.Comment]) -> Void)) {
+    detailCardCommentsViewHandler = handler
+  }
+  
+  func fetchDetailCard() {
+    let content = """
+                  안녕하세요
+                  안녕하세요
+                  """
     
+    let dueDate = "2020-11-26"
+    
+    let user1 = DetailCard.Comment.User(id: 0, name: "서명렬", profileImageUrl: "")
+    let user2 = DetailCard.Comment.User(id: 1, name: "심영민", profileImageUrl: "")
+    
+    let comment1 = DetailCard.Comment(id: 0, content: "안녕하세요", createdAt: "2020-11-26", user: user1)
+    let comment2 = DetailCard.Comment(id: 1, content: "안녕하세요", createdAt: "2020-11-26", user: user2)
+    
+    detailCardContentViewHandler?(content)
+    detailCardDueDateViewHandler?(dueDate)
+    detailCardCommentsViewHandler?([comment1, comment2])
   }
 }
