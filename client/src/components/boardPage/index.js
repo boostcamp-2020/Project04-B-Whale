939c3baae77ba453701c770a2709c571cf866e9c
HoonMac <hoon@Hoon.local>
2020-11-30 16:23:31 +0900

    [FEAT] 보드 상세페이지 내 헤더컴포넌트, 상단 메뉴바 컴포넌트 삽입 - 보드 상세페이지 내 common 헤더 컴포넌트 삽입 - 보드 상세페이지 내 상단 메뉴바 컴포넌트 삽입

--- client/src/components/boardPage/index.js
+++ client/src/components/boardPage/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
+import Header from '../common/header';
+import TopMenu from './TopMenu';
+
+const MainContents = styled.div`
+    height: 100%;
+    width: 100%;
+    background-color: ${(props) => props.backgroundColor};
+`;
 
 const Board = ({ match }) => {
     const { id } = match.params;
-    return <div>보드 상세 페이지 - boardId : {id}</div>;
+
+    return (
+        <>
+            <Header />
+            <MainContents backgroundColor="gray">
+                <TopMenu />
+                보드 상세 페이지 - boardId : {id}
+            </MainContents>
+        </>
+    );
 };
 
 export default Board;
