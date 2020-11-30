f57d087d63968f029829164ddce2af0f013640ee
HoonMac <hoon@Hoon.local>
2020-11-30 19:10:59 +0900

    [FEAT] 보드 상세페이지 내 참여자 드롭다운, 사이드바 props로 전달 - 참여자 조회 state props로 전달 - 사이드바 state props 로 전달

--- client/src/components/boardPage/index.js
+++ client/src/components/boardPage/index.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import Header from '../common/header';
 import TopMenu from './TopMenu';
+import ActivitySidebar from './ActivitySidebar';
+import InvitedDropdown from './InvitedUserDropdown';
+import { getDetailBoard } from '../../utils/boardRequest';
 
 const MainContents = styled.div`
     height: 100%;
@@ -11,13 +14,30 @@ const MainContents = styled.div`
 
 const Board = ({ match }) => {
     const { id } = match.params;
+    const [sidebarDisplay, setSidebarDisplay] = useState(false);
+    const [invitedDropdownDisplay, setInvitedDropdownDisplay] = useState(false);
+
+    useEffect(async () => {
+        const { status, data } = await getDetailBoard(id);
+        console.log(status);
+        console.log(data);
+    }, []);
 
     return (
         <>
             <Header />
             <MainContents backgroundColor="gray">
-                <TopMenu />
+                <TopMenu
+                    sidebarDisplay={sidebarDisplay}
+                    setSidebarDisplay={setSidebarDisplay}
+                    invitedDropdownDisplay={invitedDropdownDisplay}
+                    setInvitedDropdownDisplay={setInvitedDropdownDisplay}
+                />
                 보드 상세 페이지 - boardId : {id}
+                <ActivitySidebar sidebarDisplay={sidebarDisplay} />
+                {invitedDropdownDisplay && (
+                    <InvitedDropdown setInvitedDropdownDisplay={setInvitedDropdownDisplay} />
+                )}
             </MainContents>
         </>
     );
