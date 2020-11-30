6e9f737bbd8ac9990455eea090b77d95c7e0c456
수연 <cjg03194@gmail.com>
2020-11-30 17:48:48 +0900

    [FEAT] 일별 카드 개수를 조회하는 서비스 함수 추가
    - 일별 카드 개수를 조회하는 getCardCountByPeriod 함수 추가

--- server/src/service/CardService.js
+++ server/src/service/CardService.js
@@ -1,3 +1,4 @@
+import { Transactional } from 'typeorm-transactional-cls-hooked';
 import { BaseService } from './BaseService';
 
 export class CardService extends BaseService {
@@ -9,5 +10,22 @@ export class CardService extends BaseService {
         }
 
         return CardService.instance;
+    }
+
+    @Transactional()
+    async getCardCountByPeriod({ startDate, endDate, userId }) {
+        let query = this.cardRepository
+            .createQueryBuilder('card')
+            .select(`date_format(card.due_date, '%Y-%m-%d')`, 'dueDate')
+            .addSelect('count(1)', 'cardCount')
+            .where(`card.due_date BETWEEN '${startDate}' AND '${endDate}'`)
+            .groupBy(`date_format(card.due_date, '%Y-%m-%d')`);
+
+        if (userId) {
+            query = query.innerJoin('card.members', 'member', 'member.user_id=:userId', { userId });
+        }
+
+        const cardCountList = await query.getRawMany();
+        return cardCountList;
     }
 }
