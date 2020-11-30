f6da13fef32dc64b23a6b880fe166dc379981733
Seo Myung Ryul <smr3102@naver.com>
2020-11-30 15:30:31 +0900

    [FEAT] CardDetailContentLabel 구성
    - shadow 구성 및 디자인

--- iOS/AreUDone/AreUDone/CardDetailScene/View/CardDetailContentLabel.swift
+++ iOS/AreUDone/AreUDone/CardDetailScene/View/CardDetailContentLabel.swift
@@ -7,14 +7,41 @@
 
 import UIKit
 
-class CardDetailContentLabel: UILabel {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
+class CardDetailContentLabel: PaddingLabel {
+  
+  required init?(coder: NSCoder) {
+    super.init(coder: coder)
+    
+    configure()
+  }
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    
+    configure()
+  }
+  
+  private func configure() {
+    layer.borderWidth = 0.5
+    layer.borderColor = UIColor.lightGray.cgColor
+    layer.cornerRadius = 2
+    numberOfLines = 0
+    lineBreakMode = .byWordWrapping
+    font = UIFont(name: "AmericanTypewriter-Condensed", size: 15)
+    backgroundColor = .white
+    
+    
+    configureShadow()
+  }
+  
+  private func configureShadow() {
+    layer.shadowColor = UIColor.black.cgColor
+    layer.shadowOffset = CGSize(width: bounds.width + 3, height: bounds.height + 3)
+    layer.masksToBounds = false
+    layer.shadowRadius = 2
+    layer.shadowOffset = .zero
+    layer.shadowOpacity = 0.6
+    layer.shouldRasterize = true
+    layer.rasterizationScale = UIScreen.main.scale
+  }
 }
