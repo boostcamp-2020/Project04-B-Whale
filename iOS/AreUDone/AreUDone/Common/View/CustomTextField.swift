ec23f6d1de211e8564e8cffda6b171c6db7c77b8
tksrl0379 <tksrl0379@naver.com>
2020-11-30 17:44:50 +0900

    [FEAT] 추가/취소 버튼이 있는 CustomTextField 제작

---
+++ iOS/AreUDone/AreUDone/Common/View/CustomTextField.swift
@@ -0,0 +1,142 @@
+//
+//  CustomTextField.swift
+//  AreUDone
+//
+//  Created by a1111 on 2020/11/30.
+//
+
+import UIKit
+
+protocol CustomTextFieldDelegate: AnyObject {
+  func addButtonTapped(text: String)
+  func closeButtonTapped()
+}
+
+final class CustomTextField: UIView {
+  
+  // MARK: - Property
+  
+  weak var delegate: CustomTextFieldDelegate?
+  
+  private lazy var textFieldView: TextField = {
+    let textField = TextField()
+    textField.translatesAutoresizingMaskIntoConstraints = false
+    
+    textField.backgroundColor = .white
+    textField.layer.cornerRadius = 5
+    textField.layer.borderWidth = 0.2
+    
+    return textField
+  }()
+  private lazy var cancelButton: UIButton = {
+    let button = UIButton()
+    button.translatesAutoresizingMaskIntoConstraints = false
+    
+    button.setTitle("취소", for: .normal)
+    button.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
+    button.setTitleColor(.darkGray, for: .normal)
+    
+    button.addTarget(self, action: #selector(cancelButtonTapped), for: .touchUpInside)
+    
+    return button
+  }()
+  private lazy var addButton: UIButton = {
+    let button = UIButton()
+    button.translatesAutoresizingMaskIntoConstraints = false
+    
+    button.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+    button.setTitle("만들기", for: .normal)
+    button.setTitleColor(.darkGray, for: .normal)
+    
+    button.addTarget(self, action: #selector(addButtonTapped), for: .touchUpInside)
+    
+    return button
+  }()
+  
+  
+  // MARK: - Initializer
+  
+  required init?(coder: NSCoder) {
+    super.init(coder: coder)
+    
+    configure()
+  }
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    
+    configure()
+  }
+  
+  
+  // MARK: - Method
+  
+  private func configure() {
+    backgroundColor = #colorLiteral(red: 0.944453299, green: 0.9647708535, blue: 0.9688996673, alpha: 1)
+    layer.cornerRadius = 10
+    
+    addSubview(textFieldView)
+    addSubview(cancelButton)
+    addSubview(addButton)
+    
+    configureTextFieldView()
+    configureCancelButton()
+    configureAddButton()
+  }
+  
+  private func configureTextFieldView() {
+    NSLayoutConstraint.activate([
+      textFieldView.centerXAnchor.constraint(equalTo: centerXAnchor),
+      textFieldView.topAnchor.constraint(equalTo: topAnchor, constant: 5),
+      textFieldView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 5),
+      textFieldView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -5),
+      
+      textFieldView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.45)
+    ])
+  }
+  
+  private func configureCancelButton() {
+    NSLayoutConstraint.activate([
+      cancelButton.leadingAnchor.constraint(equalTo: textFieldView.leadingAnchor, constant: 10),
+      cancelButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -5),
+    ])
+  }
+  
+  private func configureAddButton() {
+    NSLayoutConstraint.activate([
+      addButton.trailingAnchor.constraint(equalTo: textFieldView.trailingAnchor, constant: -10),
+      addButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -5),
+    ])
+  }
+}
+
+extension CustomTextField {
+  
+  @objc private func addButtonTapped() {
+    delegate?.addButtonTapped(text: textFieldView.text ?? "")
+  }
+  
+  @objc private func cancelButtonTapped() {
+    delegate?.closeButtonTapped()
+  }
+}
+
+
+final class TextField: UITextField {
+  
+  // MARK: - Property
+  
+  let padding = UIEdgeInsets(top: 0, left: 5, bottom: 0, right: 5)
+  
+  override public func textRect(forBounds bounds: CGRect) -> CGRect {
+    return bounds.inset(by: padding)
+  }
+  
+  override public func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+    return bounds.inset(by: padding)
+  }
+  
+  override public func editingRect(forBounds bounds: CGRect) -> CGRect {
+    return bounds.inset(by: padding)
+  }
+}
