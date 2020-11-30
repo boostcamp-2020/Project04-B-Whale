e06dc707f50d3e0fca684b4a8a9eb9522660199a
HoonMac <hoon@Hoon.local>
2020-11-30 16:26:05 +0900

    [FEAT] 보더 상세페이지 상단 메뉴바 컴포넌트 구현 - 보더 상세페이지 상단 메뉴바 UI 구현

---
+++ client/src/components/boardPage/TopMenu.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const MenuDiv = styled.div`
+    display: flex;
+    justify-content: space-between;
+    line-height: 40px;
+    width: 100%;
+    min-height: 30px;
+    padding-left: 10%;
+`;
+
+const BoardTitle = styled.span`
+    margin-right: 5%;
+`;
+
+const ButtonForGettingInvitedUser = styled.button`
+    opacity: 0.3;
+    border-radius: 4px;
+    padding: 5px 10px;
+    margin-right: 5%;
+`;
+
+const InviteButton = styled.button`
+    opacity: 0.3;
+    border-radius: 4px;
+    padding: 5px 10px;
+`;
+
+const MenuButton = styled.button`
+    opacity: 0.3;
+    border-radius: 4px;
+    padding: 5px 10px;
+`;
+
+const TopMenu = () => {
+    return (
+        <MenuDiv>
+            <div style={{ width: '50%' }}>
+                <BoardTitle>보드이름</BoardTitle>
+                <ButtonForGettingInvitedUser>참여자 목록</ButtonForGettingInvitedUser>
+                <InviteButton>초대하기</InviteButton>
+            </div>
+            <div style={{ width: '5%' }}>
+                <MenuButton>메뉴</MenuButton>
+            </div>
+        </MenuDiv>
+    );
+};
+
+export default TopMenu;
