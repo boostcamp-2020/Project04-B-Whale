359488d3b9e1f89fdd2d785b42d32d8deac1d068
HoonMac <hoon@Hoon.local>
2020-11-30 16:25:06 +0900

    [STYLE] html, body height 변경 - html, body height 변경

--- client/src/styles/globalStyles.js
+++ client/src/styles/globalStyles.js
@@ -14,8 +14,14 @@ const GlobalStyle = createGlobalStyle`
       outline: none;
       text-decoration: none;
   }
+  html {
+    height: 100%;
+  }
   body {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    height: 100%; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+  #root{
+    height: 100%;
   }
   button {
       cursor: pointer;
