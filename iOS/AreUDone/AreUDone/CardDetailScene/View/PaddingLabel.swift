1cca9d9c54808d5a722e12ba4da863898742d405
Seo Myung Ryul <smr3102@naver.com>
2020-11-30 15:29:46 +0900

    [FEAT] PaddingLabel 추가
    - PaddingLabel 추가 및 ContentLabel에 적용

---
+++ iOS/AreUDone/AreUDone/CardDetailScene/View/PaddingLabel.swift
@@ -0,0 +1,20 @@
+//
+//  PaddingLabel.swift
+//  AreUDone
+//
+//  Created by 서명렬 on 2020/11/30.
+//
+
+import UIKit
+
+class PaddingLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
