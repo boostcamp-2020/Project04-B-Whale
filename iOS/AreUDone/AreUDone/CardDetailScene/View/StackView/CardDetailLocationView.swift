2d725053788b380866b46a8706f254fd9339944b
Seo Myung Ryul <smr3102@naver.com>
2020-12-01 13:40:32 +0900

    [FIX] 오타 수정
    - titleLable -> titleLabel

---
+++ iOS/AreUDone/AreUDone/CardDetailScene/View/StackView/CardDetailLocationView.swift
@@ -0,0 +1,20 @@
+//
+//  CardDetailLocationView.swift
+//  AreUDone
+//
+//  Created by 서명렬 on 2020/12/01.
+//
+
+import UIKit
+
+class CardDetailLocationView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
