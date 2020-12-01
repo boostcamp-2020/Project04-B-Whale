1aabf2e5ea40ba584b308c00f72088ed7dc978d7
youngxpepp <youngxpepp@gmail.com>
2020-12-01 09:55:53 +0900

    [FIX] 테스트 종료 시 데이터베이스 커넥션을 함께 종료
    - 테스트가 끝나도 특정 비동기 동작이 수행 중 -> 데이터베이스 커넥션
    - 테스트 afterAll에 커넥션 종료 명령 추가

--- server/src/Application.js
+++ server/src/Application.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import path from 'path';
 import { validateOrReject } from 'class-validator';
-import { createConnection } from 'typeorm';
+import { createConnection, getConnection } from 'typeorm';
 import {
     initializeTransactionalContext,
     patchTypeORMRepositoryWithBaseRepository,
@@ -72,5 +72,9 @@ export class Application {
     initPassport() {
         passport.use(new NaverStrategy());
         passport.use(new JwtStrategy());
+    }
+
+    async close() {
+        await getConnection().close();
     }
 }
