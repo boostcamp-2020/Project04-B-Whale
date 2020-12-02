63588a6036c42ae827e8a12d4af87e7fc9752cdc
수연 <cjg03194@gmail.com>
2020-12-02 16:39:33 +0900

    [FEAT] 일별 카드 개수 조회 API validation 체크 로직 추가
    - 일별 카드 개수 조회 API의 controller 부분에 validation 체크 로직 추가

--- server/src/router/CardRouter.js
+++ server/src/router/CardRouter.js
@@ -1,19 +1,22 @@
 import { Router } from 'express';
 import { CardService } from '../service/CardService';
 import { queryParser } from '../common/util/queryParser';
+import { validator } from '../common/util/validator';
+import { CardCountDto } from '../dto/CardCountDto';
 
 export const CardRouter = () => {
     const router = Router();
 
     router.get('/count', async (req, res) => {
-        const cardService = CardService.getInstance();
-        // TODO : validation 체크 로직 추가 할 것
+        await validator(CardCountDto, req.query, ['isQuery']);
 
+        const cardService = CardService.getInstance();
         const { q } = req.query;
         const { id: userId } = req.user;
         const { startdate: startDate, enddate: endDate, member } = queryParser(q);
-
         const config = { startDate, endDate, userId, member };
+
+        await validator(CardCountDto, config, ['queryValue']);
 
         const cardCounts = await cardService.getCardCountByPeriod(config);
         res.status(200).json({ cardCounts });
