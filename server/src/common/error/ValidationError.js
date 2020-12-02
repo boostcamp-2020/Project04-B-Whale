90e7527fc73a87def58ae369ec9e3e23f66ab30e
수연 <cjg03194@gmail.com>
2020-12-02 19:35:13 +0900

    [FIX] 리뷰에 따라 코드 수정
    - validate함수 대신 validateOrReject 함수 사용으로 변경
    - validateOrReject 함수 사용에 따라 ValidationError 클래스 제거

--- server/src/common/error/ValidationError.js
+++
@@ -1,8 +0,0 @@
-import { BusinessError } from './BusinessError';
-import { ErrorCode } from './ErrorCode';
-
-export class ValidationError extends BusinessError {
-    constructor(message) {
-        super(ErrorCode.VALIDATION_ERROR, message);
-    }
-}
--- server/src/common/util/validator.js
+++ server/src/common/util/validator.js
@@ -1,11 +1,7 @@
 import { plainToClass } from 'class-transformer';
-import { validate } from 'class-validator';
-import { ValidationError } from '../error/ValidationError';
+import { validateOrReject } from 'class-validator';
 
 export const validator = async (DtoClass, Object, groups) => {
     const classObject = plainToClass(DtoClass, Object);
-    const [error] = await validate(classObject, { groups });
-    if (error) {
-        throw new ValidationError('validation failed');
-    }
+    await validateOrReject(classObject, { groups });
 };
