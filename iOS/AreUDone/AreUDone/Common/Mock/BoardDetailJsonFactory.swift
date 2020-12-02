2903653ba18bf2089bc1ddd42a1d6bda0cfaefa3
tksrl0379 <tksrl0379@naver.com>
2020-12-02 18:45:10 +0900

    [FEAT] 보드 상세 목록 조회 Mock 데이터 추가
    - 보드 상세 목록 조회 Mock 데이터 추가

--- iOS/AreUDone/AreUDone/Common/Mock/BoardDetailJsonFactory.swift
+++ iOS/AreUDone/AreUDone/Common/Mock/BoardDetailJsonFactory.swift
@@ -43,10 +43,142 @@ struct BoardDetailTrueJsonFactory: JsonFactory {
                           "dueDate": "2020-12-02"
                       }
                   ]
+              },
+              {
+                  "id": 1,
+                  "title": "리스트 제목2",
+                  "position": 1,
+                  "cards": [
+                      {
+                          "id": 0,
+                          "title": "카드 제목2",
+                          "position": 0,
+                          "commentCount": 5,
+                          "dueDate": "2020-12-02"
+                      },
+                      {
+                          "id": 0,
+                          "title": "카드 제목3",
+                          "position": 0,
+                          "commentCount": 5,
+                          "dueDate": "2020-12-02"
+                      },
+                      {
+                          "id": 0,
+                          "title": "카드 제목4",
+                          "position": 0,
+                          "commentCount": 5,
+                          "dueDate": "2020-12-02"
+                      },
+                      {
+                          "id": 0,
+                          "title": "카드 제목5",
+                          "position": 0,
+                          "commentCount": 5,
+                          "dueDate": "2020-12-02"
+                      },
+                      {
+                          "id": 0,
+                          "title": "카드 제목6",
+                          "position": 0,
+                          "commentCount": 5,
+                          "dueDate": "2020-12-02"
+                      },
+                      {
+                          "id": 0,
+                          "title": "카드 제목7",
+                          "position": 0,
+                          "commentCount": 5,
+                          "dueDate": "2020-12-02"
+                      },
+                      {
+                          "id": 0,
+                          "title": "카드 제목8",
+                          "position": 0,
+                          "commentCount": 5,
+                          "dueDate": "2020-12-02"
+                      },
+                      {
+                          "id": 0,
+                          "title": "카드 제목9",
+                          "position": 0,
+                          "commentCount": 5,
+                          "dueDate": "2020-12-02"
+                      },
+                      {
+                          "id": 0,
+                          "title": "카드 제목10",
+                          "position": 0,
+                          "commentCount": 5,
+                          "dueDate": "2020-12-02"
+                      },
+                      {
+                          "id": 0,
+                          "title": "카드 제목11",
+                          "position": 0,
+                          "commentCount": 5,
+                          "dueDate": "2020-12-02"
+                      },
+                      {
+                          "id": 0,
+                          "title": "카드 제목12",
+                          "position": 0,
+                          "commentCount": 5,
+                          "dueDate": "2020-12-02"
+                      },
+                      {
+                          "id": 0,
+                          "title": "카드 제목13",
+                          "position": 0,
+                          "commentCount": 5,
+                          "dueDate": "2020-12-02"
+                      },
+                      {
+                          "id": 0,
+                          "title": "카드 제목14",
+                          "position": 0,
+                          "commentCount": 5,
+                          "dueDate": "2020-12-02"
+                      },
+                      {
+                          "id": 0,
+                          "title": "카드 제목15",
+                          "position": 0,
+                          "commentCount": 5,
+                          "dueDate": "2020-12-02"
+                      },
+                      {
+                          "id": 0,
+                          "title": "카드 제목16",
+                          "position": 0,
+                          "commentCount": 5,
+                          "dueDate": "2020-12-02"
+                      },
+                      {
+                          "id": 0,
+                          "title": "카드 제목17",
+                          "position": 0,
+                          "commentCount": 5,
+                          "dueDate": "2020-12-02"
+                      },
+                      {
+                          "id": 0,
+                          "title": "카드 제목18",
+                          "position": 0,
+                          "commentCount": 5,
+                          "dueDate": "2020-12-02"
+                      },
+                      {
+                          "id": 0,
+                          "title": "카드 제목19",
+                          "position": 0,
+                          "commentCount": 5,
+                          "dueDate": "2020-12-02"
+                      }
+                  ]
               }
           ]
       }
-
       """.data(using: .utf8)
     default:
       return nil
