f82dd11e09ce800b243122e34507046ff0e11a40
HoonMac <hoon@Hoon.local>
2020-12-02 23:28:10 +0900

    [FEAT] 보드 상세조회 페이지 내에서 context 데이터 사용할 수 있도록 구현 - 보드 상세 페이지에서 사용 할 provider 로 감싸줌 - 보드 상세 컴포넌트의 자식 컴포넌트들에서 사용할 수 있도록 구현

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,16 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Main from './components/mainPage';
 import Login from './components/loginPage';
 import Board from './components/boardPage';
+import BoardDetailProvider from './components/provider/BoardDetailProvider';
 
 const App = () => {
     return (
         <BrowserRouter>
             <Route exact path="/" component={Main} />
             <Route exact path="/login" component={Login} />
-            <Route exact path="/board/:id" component={Board} />
+            <BoardDetailProvider>
+                <Route exact path="/board/:id" component={Board} />
+            </BoardDetailProvider>
         </BrowserRouter>
     );
 };
