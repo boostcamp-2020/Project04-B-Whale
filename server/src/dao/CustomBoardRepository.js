78f6110c3dd26e4f3ff25db43cda34e021b7b175
youngxpepp <youngxpepp@gmail.com>
2020-12-03 17:39:38 +0900

    [FEAT] 일별 카드 조회 API의 Dao 메서드 생성
    - CustomCardRepository와 CustomBoardRepository 생성
    - dueDate와 카드 생성자를 통해 카드를 조회하는 메서드 추가
    - dueDate와 카드에 할당된 사용자 id를 통해 카드를 조회하는 메서드 추가
    - dueDate와 카드가 포함된 보드 id 배열을 통해 카드를 조회하는 메서드 추가
    - 보드 생성자 id를 통해서 보드를 조회하는 메서드 추가
    - 특정 사용자 id를 통해 초대된 보드를 조회하는 메서드 추가
    - 메서드에 대한 단위 테스트

---
+++ server/src/dao/CustomBoardRepository.js
@@ -0,0 +1,19 @@
+import { EntityRepository } from 'typeorm';
+import { BaseRepository, Transactional } from 'typeorm-transactional-cls-hooked';
+import { Board } from '../model/Board';
+
+@EntityRepository(Board)
+export class CustomBoardRepository extends BaseRepository {
+    async findByCreatorId(creatorId) {
+        const boards = await this.find({ creator: { id: creatorId } });
+        return boards;
+    }
+
+    async findInvitedBoardsByUserId(userId) {
+        const boards = await this.createQueryBuilder('a')
+            .innerJoin('a.invitations', 'b')
+            .where('b.user.id = :userId', { userId })
+            .getMany();
+        return boards;
+    }
+}
