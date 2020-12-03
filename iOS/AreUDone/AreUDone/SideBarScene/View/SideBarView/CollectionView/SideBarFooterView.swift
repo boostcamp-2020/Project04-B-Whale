5c21b278411d47136406b9bcb9529276805fbee3
tksrl0379 <tksrl0379@naver.com>
2020-12-03 19:53:54 +0900

    [FEAT] 사이드바 버튼 추가
    - 사이드바 버튼 추가

--- iOS/AreUDone/AreUDone/SideBarScene/View/SideBarView/CollectionView/SideBarFooterView.swift
+++ iOS/AreUDone/AreUDone/SideBarScene/View/SideBarView/CollectionView/SideBarFooterView.swift
@@ -7,14 +7,32 @@
 
 import UIKit
 
+protocol SideBarFooterViewDelegate: AnyObject {
+  
+  func baseViewTapped()
+}
+
 final class SideBarFooterView: UICollectionReusableView, Reusable {
   
   // MARK: - Property
   
+  weak var delegate: SideBarFooterViewDelegate?
+  
+  private lazy var baseView: UIView = {
+    let view = UIView()
+    view.translatesAutoresizingMaskIntoConstraints = false
+    
+    view.backgroundColor = .white
+    view.layer.cornerRadius = 10
+    view.layer.borderColor = UIColor.black.cgColor
+    
+    return view
+  }()
+  
   private lazy var titleLabel: UILabel = {
     let titleLabel = UILabel()
     titleLabel.translatesAutoresizingMaskIntoConstraints = false
-
+    
     return titleLabel
   }()
   
@@ -29,8 +47,16 @@ final class SideBarFooterView: UICollectionReusableView, Reusable {
   
   override init(frame: CGRect) {
     super.init(frame: frame)
-    backgroundColor = .red
     configure()
+  }
+  
+  
+  // MARK: - Method
+  
+  func update(with title: String) {
+    titleLabel.text = title
+    if title.isEmpty { baseView.layer.borderWidth = 0 }
+    else { baseView.layer.borderWidth = 1 }
   }
 }
 
@@ -40,14 +66,40 @@ final class SideBarFooterView: UICollectionReusableView, Reusable {
 private extension SideBarFooterView {
   
   func configure() {
-    addSubview(titleLabel)
     
+    addSubview(baseView)
+    baseView.addSubview(titleLabel)
+    
+    configureBaseView()
     configureTitleLabel()
+  }
+  
+  func configureBaseView() {
+    NSLayoutConstraint.activate([
+      baseView.centerXAnchor.constraint(equalTo: centerXAnchor),
+      baseView.topAnchor.constraint(equalTo: topAnchor, constant: 15),
+      baseView.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.95),
+      baseView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -15)
+    ])
+    
+    let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(baseViewTapped))
+    baseView.addGestureRecognizer(gestureRecognizer)
   }
   
   func configureTitleLabel() {
     NSLayoutConstraint.activate([
-      
+      titleLabel.centerXAnchor.constraint(equalTo: baseView.centerXAnchor),
+      titleLabel.centerYAnchor.constraint(equalTo: baseView.centerYAnchor)
     ])
+  }
+}
+
+
+// MARK: - Extension objc
+
+private extension SideBarFooterView {
+  
+  @objc func baseViewTapped() {
+    delegate?.baseViewTapped()
   }
 }
