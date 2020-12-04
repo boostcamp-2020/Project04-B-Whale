09b30d6285279bfcf3a42cdad8cec0bd36d48507
HoonMac <hoon@Hoon.local>
2020-12-04 10:26:59 +0900

    [FEAT] 보드 타이틀 부분 클릭 시 input창으로 변경 구현 - 보드 상세페이지 내 타이틀 클릭 시 타이틀 변경 가능한 input컴포넌트 변경

--- client/src/components/boardPage/TopMenu.js
+++ client/src/components/boardPage/TopMenu.js
@@ -1,4 +1,5 @@
-import React, { useContext } from 'react';
+/* eslint-disable jsx-a11y/no-autofocus */
+import React, { useContext, useState } from 'react';
 import styled from 'styled-components';
 import BoardDetailContext from '../../context/BoardDetailContext';
 
@@ -13,6 +14,22 @@ const MenuDiv = styled.div`
 
 const BoardTitle = styled.span`
     margin-right: 5%;
+    border-radius: 4px;
+    padding: 5px 5px;
+    resize: none;
+    ${(props) =>
+        props.state === 'span' &&
+        `&:hover {
+        cursor: pointer;
+        opacity: 0.3;
+        background-color: gray;
+    }`};
+`;
+
+const BoardInput = styled.input`
+    width: ${(props) => (props.width <= 0 ? 20 : props.width * 10)}px;
+    height: 30px;
+    border-radius: 4px;
 `;
 
 const ButtonForGettingInvitedUser = styled.button`
@@ -35,12 +52,35 @@ const MenuButton = styled.button`
 `;
 
 const TopMenu = (props) => {
-    const { boardDetail } = useContext(BoardDetailContext);
+    const { boardDetail, setBoardDetail } = useContext(BoardDetailContext);
+    const [inputState, setInputState] = useState('span');
 
+    const boardTitleClickHandler = () => {
+        setInputState('input');
+    };
+    const changeBoardTitle = (evt) => {
+        if (evt.keyCode === 13) {
+            // 이름 변경api, boardDetailcontext 이름 변경
+            console.log(boardDetail.title);
+        }
+    };
     return (
         <MenuDiv>
             <div style={{ width: '50%' }}>
-                <BoardTitle>{boardDetail.title}</BoardTitle>
+                <BoardTitle state={inputState} onClick={boardTitleClickHandler}>
+                    {inputState === 'span' && boardDetail.title}
+                    {inputState === 'input' && (
+                        <BoardInput
+                            width={boardDetail.title.length}
+                            value={boardDetail.title}
+                            onChange={(evt) =>
+                                setBoardDetail({ ...boardDetail, title: evt.target.value })
+                            }
+                            autoFocus="autoFocus"
+                            onKeyDown={changeBoardTitle}
+                        />
+                    )}
+                </BoardTitle>
                 <ButtonForGettingInvitedUser
                     onClick={(evt) =>
                         props.setInvitedDropdownDisplay({
