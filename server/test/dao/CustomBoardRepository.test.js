77c197123aaa74b34ffd1f484636ab78e49f3406
youngxpepp <youngxpepp@gmail.com>
2020-12-04 14:41:15 +0900

    일별 카드 조회 API 구현 완료
    * [FEAT] 일별 카드 조회 API의 Dao 메서드 생성
    
    - CustomCardRepository와 CustomBoardRepository 생성
    - dueDate와 카드 생성자를 통해 카드를 조회하는 메서드 추가
    - dueDate와 카드에 할당된 사용자 id를 통해 카드를 조회하는 메서드 추가
    - dueDate와 카드가 포함된 보드 id 배열을 통해 카드를 조회하는 메서드 추가
    - 보드 생성자 id를 통해서 보드를 조회하는 메서드 추가
    - 특정 사용자 id를 통해 초대된 보드를 조회하는 메서드 추가
    - 메서드에 대한 단위 테스트
    
    * [CHORE] 사용하지 않는 변수 제거
    
    - import 하고 나서 사용하지 않는 변수 또는 함수를 제거함
    
    * [FEAT] CardService에 일별 카드 조회 메서드 추가
    
    - getMyCardsByDueDate: 특정 날짜에 본인이 생성하거나 할당된 카드들을 조회
    - getAllCardsByDueDate: 특정 날짜에 본인과 연관된 보드들의 모든 카드들을 조회
    - 각 쿼리에 commentCount를 추가
    - 테스트 작성
    
    * [FEAT] 일별 카드 조회 API 구현
    
    - 라우터 추가
    - API 요청 dto 추가
    - moment 타임존 의존성 추가
    - 테스트 작성

---
+++ server/test/dao/CustomBoardRepository.test.js
@@ -0,0 +1,103 @@
+import { getCustomRepository } from 'typeorm';
+import { getEntityManagerOrTransactionManager } from 'typeorm-transactional-cls-hooked';
+import { Application } from '../../src/Application';
+import { CustomBoardRepository } from '../../src/dao/CustomBoardRepository';
+import { Board } from '../../src/model/Board';
+import { Invitation } from '../../src/model/Invitation';
+import { User } from '../../src/model/User';
+import { TestTransactionDelegate } from '../TestTransactionDelegate';
+
+describe('CustomBoardRepository', () => {
+    const app = new Application();
+
+    beforeAll(async () => {
+        await app.initialize();
+    });
+
+    afterAll(async (done) => {
+        await app.close();
+        done();
+    });
+
+    test('findByCreatorId(): user0이 만든 board 조회', async () => {
+        const repo = getCustomRepository(CustomBoardRepository);
+        await TestTransactionDelegate.transaction(async () => {
+            // given
+            const em = getEntityManagerOrTransactionManager('default');
+            const user0 = em.create(User, {
+                socialId: '0',
+                name: 'youngxpepp',
+                profileImageUrl: 'http://',
+            });
+            await em.save(user0);
+            const board0 = em.create(Board, {
+                title: 'board title 0',
+                color: '#FFFFFF',
+                creator: user0,
+            });
+            await em.save(board0);
+
+            // when
+            const boards = await repo.findByCreatorId(user0.id);
+
+            // then
+            expect(boards?.[0].id).toEqual(board0.id);
+        });
+    });
+
+    test('findInvitedBoardsByUserId(): user0의 초대된 board 조회', async () => {
+        const repo = getCustomRepository(CustomBoardRepository);
+        await TestTransactionDelegate.transaction(async () => {
+            // given
+            const em = getEntityManagerOrTransactionManager('default');
+            const user0 = em.create(User, {
+                socialId: '0',
+                name: 'youngxpepp',
+                profileImageUrl: 'http://',
+            });
+            const user1 = em.create(User, {
+                socialId: '1',
+                name: 'dhoon',
+                profileImageUrl: 'http://',
+            });
+            await em.save([user0, user1]);
+            const board0 = em.create(Board, {
+                title: 'board title 0',
+                color: '#FFFFFF',
+                creator: user1,
+            });
+            const board1 = em.create(Board, {
+                title: 'board title 1',
+                color: '#FFFFFF',
+                creator: user1,
+            });
+            const board2 = em.create(Board, {
+                title: 'board title 2',
+                color: '#FFFFFF',
+                creator: user1,
+            });
+            await em.save([board0, board1, board2]);
+            await em.save([
+                em.create(Invitation, {
+                    user: user0,
+                    board: board0,
+                }),
+                em.create(Invitation, {
+                    user: user0,
+                    board: board1,
+                }),
+                em.create(Invitation, {
+                    user: user0,
+                    board: board2,
+                }),
+            ]);
+
+            // when
+            const boards = await repo.findInvitedBoardsByUserId(user0.id);
+
+            // then
+            expect(boards).toHaveLength(3);
+            expect(boards.map((board) => board.id)).toContain(board0.id, board1.id, board2.id);
+        });
+    });
+});
