34926eee46da054e52ca66063323dfda8cacbf5e
HoonMac <hoon@Hoon.local>
2020-12-04 11:03:31 +0900

    [STYLE] 로그인 페이지 메인 로고 추가, 가운데 정렬 css 변경

--- client/src/components/loginPage/index.js
+++ client/src/components/loginPage/index.js
@@ -1,36 +1,34 @@
 import React from 'react';
+import styled from 'styled-components';
 import NaverLoginButton from './NaverLoginButton';
 import backgroundVideo from '../../image/background.mp4';
+import logo from '../../image/app_logo.png';
+
+const LoginPageWrapper = styled.div`
+    width: '100%';
+    height: '100%';
+    overflow: 'hidden';
+    margin: '0px auto';
+    position: 'absolute';
+`;
+
+const LogoImg = styled.img`
+    position: absolute;
+    top: 20%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+`;
 
 const Login = () => {
     return (
         <>
-            <div
-                style={{
-                    width: '100%',
-                    height: '100%',
-                    overflow: 'hidden',
-                    margin: '0px auto',
-                    position: 'relative',
-                }}
-            >
+            <LoginPageWrapper>
                 <video muted autoPlay loop style={{ width: '100%', height: '100%' }}>
                     <source src={backgroundVideo} type="video/mp4" />
                 </video>
-                <p
-                    style={{
-                        position: 'absolute',
-                        top: '10%',
-                        left: '43%',
-                        textAlign: 'center',
-                        fontSize: '48px',
-                        color: '#f73f52',
-                    }}
-                >
-                    AreUDone
-                </p>
+                <LogoImg src={logo} alt="naver login" />
                 <NaverLoginButton />
-            </div>
+            </LoginPageWrapper>
         </>
     );
 };
