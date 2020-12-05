0eaf67f4d30c4e72f070b20d53e39f7ce197b7e6
HoonMac <hoon@Hoon.local>
2020-12-05 15:19:28 +0900

    [FEAT] customUserRepository 추가 - 유저 id를 통한 userName 사용 위한 finduserNameById메서드 추가

---
+++ server/src/dao/CustomUserRepository.js
@@ -0,0 +1,11 @@
+import { EntityRepository } from 'typeorm';
+import { BaseRepository } from 'typeorm-transactional-cls-hooked';
+import { User } from '../model/User';
+
+@EntityRepository(User)
+export class CustomUserRepository extends BaseRepository {
+    async finduserNameById(userId) {
+        const boards = await this.findOne(userId);
+        return boards.name;
+    }
+}
