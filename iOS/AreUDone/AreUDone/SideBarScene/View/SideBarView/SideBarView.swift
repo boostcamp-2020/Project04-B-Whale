3e38c2e760202bf571e12f139c96dc06188eacab
tksrl0379 <tksrl0379@naver.com>
2020-12-05 14:03:15 +0900

    [FIX] ContentView 때문에 초대하기 버튼 안 눌리는 버그 수정
    - ContentView 때문에 초대하기 버튼 안 눌리는 버그 수정

--- iOS/AreUDone/AreUDone/SideBarScene/View/SideBarView/SideBarView.swift
+++ iOS/AreUDone/AreUDone/SideBarScene/View/SideBarView/SideBarView.swift
@@ -66,6 +66,9 @@ private extension SideBarView {
   
   func configureView() {
     backgroundColor = .white
+    layer.cornerRadius = 10
+    layer.masksToBounds = true
+    layer.maskedCorners = [.layerMinXMinYCorner, .layerMinXMaxYCorner]
   }
   
   func configureTitleView() {
