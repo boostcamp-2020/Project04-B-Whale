7a8787a860b6d1495549566915754ab1d3d19b82
HoonMac <hoon@Hoon.local>
2020-12-07 15:24:08 +0900

    [FEAT] 리스트 생성 서비스함수 구현 - 리스트 생성 서비스 함수 구현 - 보드 존재 검사, 권한검사 추가

--- server/src/service/ListService.js
+++ server/src/service/ListService.js
@@ -1,4 +1,7 @@
+import { Transactional } from 'typeorm-transactional-cls-hooked';
 import { BaseService } from './BaseService';
+import { EntityNotFoundError } from '../common/error/EntityNotFoundError';
+import { BoardService } from './BoardService';
 
 export class ListService extends BaseService {
     static instance = null;
@@ -9,5 +12,24 @@ export class ListService extends BaseService {
         }
 
         return ListService.instance;
+    }
+
+    @Transactional()
+    async createList(userId, boardId, position, title) {
+        const boardService = BoardService.getInstance();
+        const board = await this.boardRepository.findOne({
+            select: ['id'],
+            where: { id: boardId },
+        });
+        if (!board) throw new EntityNotFoundError();
+        await boardService.checkForbidden(userId, boardId);
+        const list = {
+            creator: userId,
+            board: boardId,
+            position,
+            title,
+        };
+        const createList = this.listRepository.create(list);
+        await this.listRepository.save(createList);
     }
 }
