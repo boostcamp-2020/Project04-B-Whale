a2068a16bfe0e9321ebfa2a9aa0833d0c745647e
Sooyeon Park <49746644+Park-SooYeon@users.noreply.github.com>
2020-12-07 12:03:47 +0900

    me checkbox component 구현
    * [FEAT] Me checkbox component 추가
    
    - me checkbox component 추가
    - default 값은 선택
    
    * [FEAT] me checkbox change 이벤트 추가
    
    - me checkbox의 상태가 변경될 때마다 달력에 나타낼 데이터를 가져오도록 change 이벤트 추가
    - context의 CHANGE_MEMBER dispatch 함수 추가
    - getCardCount 요청 함수에 member 변수 추가
    
    * [FEAT] 함수가 변경됨에 따라 호출시 매개변수 추가
    
    - getCardCount가 변경됨에 따라 해당 함수 호출시 필요한 매개변수 추가

--- client/src/utils/cardRequest.js
+++ client/src/utils/cardRequest.js
@@ -1,9 +1,11 @@
 import request from './api';
 
 // eslint-disable-next-line import/prefer-default-export
-export const getCardCount = async ({ startDate, endDate }) => {
+export const getCardCount = async ({ startDate, endDate, member }) => {
     const config = {
-        url: `/api/card/count?q=startdate:${startDate} enddate:${endDate}`,
+        url: member
+            ? `/api/card/count?q=startdate:${startDate} enddate:${endDate} member:${member}`
+            : `/api/card/count?q=startdate:${startDate} enddate:${endDate}`,
         method: 'GET',
     };
     const response = await request(config);
