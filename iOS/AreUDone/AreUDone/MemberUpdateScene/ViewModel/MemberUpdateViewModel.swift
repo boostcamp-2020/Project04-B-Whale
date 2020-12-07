9616e76bc75b2796d1c3899a931052ea9baf92af
Seo Myung Ryul <smr3102@naver.com>
2020-12-07 21:38:02 +0900

    [FEAT] MemberUpdateViewModel 구현
    - BoardMember와 CardMember를 Set으로 연산해 completionHandler를 사용하는 메서드 구현

--- iOS/AreUDone/AreUDone/MemberUpdateScene/ViewModel/MemberUpdateViewModel.swift
+++ iOS/AreUDone/AreUDone/MemberUpdateScene/ViewModel/MemberUpdateViewModel.swift
@@ -9,8 +9,40 @@ import Foundation
 
 protocol MemberUpdateViewModelProtocol {
   
+  func fetchMemberData(completionHandler: @escaping (([InvitedUser], [InvitedUser]?) -> Void))
 }
 
 final class MemberUpdateViewModel: MemberUpdateViewModelProtocol {
   
+  // MARK:- Property
+  
+  private let boardId: Int
+  private let cardMember: [InvitedUser]?
+  private let boardService: BoardServiceProtocol
+  
+  // MARK:- Initializer
+  
+  init(boardId: Int, cardMember: [InvitedUser]?, boardService: BoardServiceProtocol) {
+    self.boardId = boardId
+    self.cardMember = cardMember
+    self.boardService = boardService
+  }
+  
+  func fetchMemberData(completionHandler: @escaping (([InvitedUser], [InvitedUser]?) -> Void)) {
+    boardService.fetchBoardDetail(with: boardId) { result in
+      switch result {
+      case .success(let boardDetail):
+        let boardMember = boardDetail.invitedUsers
+        guard let cardMember = self.cardMember else {
+          completionHandler(boardMember, nil)
+          return
+        }
+
+        let notCardMember = Set(boardMember).subtracting(cardMember).sorted { $0.id < $1.id }
+        completionHandler(notCardMember, cardMember)
+      case .failure(let error):
+        print(error)
+      }
+    }
+  }
 }
