4b39e6d9db0767a59a1b57690b64ef63620bfcf7
Seo Myung Ryul <smr3102@naver.com>
2020-12-08 14:20:58 +0900

    [FEAT] Delete 경고를 나타내는 alert 추가
    - Delete 경고를 나타내는 alert 추가

--- iOS/AreUDone/AreUDone/Common/AlertFactory.swift
+++ iOS/AreUDone/AreUDone/Common/AlertFactory.swift
@@ -17,6 +17,7 @@ protocol AlertProtocol {
 
 enum AlertType {
   case dataLoss
+  case delete
 }
 
 class AlertFactory {
@@ -25,6 +26,8 @@ class AlertFactory {
     var alert: AlertProtocol
     switch type {
     case .dataLoss: alert = DataLossAlert(style: style)
+      
+    case .delete: alert = DeleteAlert(style: style)
     }
     
     return alert
@@ -37,5 +40,14 @@ struct DataLossAlert: AlertProtocol {
   var message: String? { return nil }
   var actionTitle: String? { return "버리기" }
   var cancelTitle: String? { return "취소" }
+  var style: UIAlertController.Style
+}
+
+struct DeleteAlert: AlertProtocol {
+  
+  var title: String? { return "해당 댓글을 삭제하시겠습니까?"}
+  var message: String? { return nil }
+  var actionTitle: String? { return "삭제"}
+  var cancelTitle: String? { return "취소"}
   var style: UIAlertController.Style
 }
