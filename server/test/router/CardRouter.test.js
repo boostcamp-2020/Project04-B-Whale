7a20478f7a91a1b801bd247b0fcf15bd32083d2f
donghoon <0930dhs@gmail.com>
2020-12-09 00:16:32 +0900

    [TEST] 카드 생성, 삭제 라우터 및 서비스 함수 테스트 구현 - 카드 생성, 삭제 api  라우터, 서비스 함수 테스트 코드 작성

--- server/test/router/CardRouter.test.js
+++ server/test/router/CardRouter.test.js
@@ -1323,4 +1323,80 @@ describe('Board API Test', () => {
             });
         });
     });
+
+    test('DELETE /api/card/:id 카드 삭제api 호출할 때, 카드 미존재시 404을 리턴한다.', async () => {
+        await TestTransactionDelegate.transaction(async () => {
+            // given
+            const user = { name: 'user', socialId: '1234', profileImageUrl: 'image' };
+            const userRepository = getRepository(User);
+            const createUser = userRepository.create(user);
+            await userRepository.save(createUser);
+            const token = await jwtUtil.generateAccessToken({
+                userId: createUser.id,
+                username: createUser.name,
+            });
+            const cardId = 0;
+            // when
+            const response = await request(app.httpServer).delete(`/api/card/${cardId}`).set({
+                Authorization: token,
+                'Content-Type': 'application/json',
+            });
+
+            // then
+            expect(response.status).toEqual(404);
+        });
+    });
+
+    test('DELETE /api/card/:id 카드 삭제api 호출할 때, 정상 호출 시 204을 리턴한다.', async () => {
+        await TestTransactionDelegate.transaction(async () => {
+            // given
+            const user = { name: 'dh', socialId: '1234', profileImageUrl: 'image' };
+            const userRepository = getRepository(User);
+            const createUser = userRepository.create(user);
+            await userRepository.save(createUser);
+            const token = await jwtUtil.generateAccessToken({
+                userId: createUser.id,
+                username: createUser.name,
+            });
+
+            const boardRepository = getRepository(Board);
+            const board = { title: 'board', creator: createUser.id, color: '#ffffff' };
+            const createBoard = boardRepository.create(board);
+            await boardRepository.save(createBoard);
+
+            const listRepository = getRepository(List);
+            const list = {
+                title: 'list',
+                creator: createUser.id,
+                position: 1,
+                board: createBoard.id,
+            };
+            const createList = listRepository.create(list);
+            await listRepository.save(createList);
+
+            const cardRepository = getRepository(Card);
+            const card = {
+                title: 'card',
+                creator: createUser.id,
+                position: 1,
+                content: 'card detail',
+                dueDate: '2020-12-31 00:00:00',
+                list: createList.id,
+                board: createBoard.id,
+            };
+            const createCard = cardRepository.create(card);
+            await cardRepository.save(createCard);
+
+            // when
+            const response = await request(app.httpServer)
+                .delete(`/api/card/${createCard.id}`)
+                .set({
+                    Authorization: token,
+                    'Content-Type': 'application/json',
+                });
+
+            // then
+            expect(response.status).toEqual(204);
+        });
+    });
 });
--- server/test/router/ListRouter.test.js
+++ server/test/router/ListRouter.test.js
@@ -148,4 +148,74 @@ describe('Board API Test', () => {
             expect(response.status).toEqual(204);
         });
     });
+
+    test('POST /api/list/{listId}/card 카드 추가 api 호출 시, 리스트 존재하지 않을 때, 404 반환', async () => {
+        await TestTransactionDelegate.transaction(async () => {
+            // given
+            const user = { name: 'dhoon', socialId: '1234', profileImageUrl: 'image' };
+            const userRepository = getRepository(User);
+            const createUser = userRepository.create(user);
+            const createdUser = await userRepository.save(createUser);
+            const token = await jwtUtil.generateAccessToken({
+                userId: createdUser.id,
+                username: createdUser.name,
+            });
+            const listId = 0;
+
+            // when
+            const response = await request(app.httpServer).post(`/api/list/${listId}/card`).set({
+                Authorization: token,
+                'Content-Type': 'application/json',
+            });
+
+            // then
+            expect(response.status).toEqual(404);
+        });
+    });
+
+    test('POST /api/list/{listId}/card 카드 추가 api 호출 시, 정상호출 시, 201 반환', async () => {
+        await TestTransactionDelegate.transaction(async () => {
+            // given
+            const user = { name: 'dhoon', socialId: '1234', profileImageUrl: 'image' };
+            const userRepository = getRepository(User);
+            const createUser = userRepository.create(user);
+            const createdUser = await userRepository.save(createUser);
+            const token = await jwtUtil.generateAccessToken({
+                userId: createdUser.id,
+                username: createdUser.name,
+            });
+
+            const boardRepository = getRepository(Board);
+            const board = { title: 'board', creator: createUser.id, color: '#ffffff' };
+            const createBoard = boardRepository.create(board);
+            await boardRepository.save(createBoard);
+
+            const listRepository = getRepository(List);
+            const list = {
+                title: 'list',
+                creator: createUser.id,
+                position: 1,
+                board: createBoard.id,
+            };
+            const createList = listRepository.create(list);
+            await listRepository.save(createList);
+
+            // when
+            const response = await request(app.httpServer)
+                .post(`/api/list/${createList.id}/card`)
+                .set({
+                    Authorization: token,
+                    'Content-Type': 'application/json',
+                })
+                .send({
+                    title: '카드 추가',
+                    dueDate: '2020-12-31 00:00:00',
+                    position: 1,
+                    content: '카드 상세',
+                });
+
+            // then
+            expect(response.status).toEqual(201);
+        });
+    });
 });
