967cd1d60633608e4e2c00845324bacc705f5654
tksrl0379 <tksrl0379@naver.com>
2020-12-09 12:12:08 +0900

    [REFACTOR] BoardDetailCollectionViewCell 일부 코드 모듈화
    - DataSource 분리
    - collectionView 분리

---
+++ iOS/AreUDone/AreUDone/BoardDetailScene/View/CollectionView/ListCollectionViewDataSource.swift
@@ -0,0 +1,58 @@
+//
+//  ListCollectionViewDataSource.swift
+//  AreUDone
+//
+//  Created by a1111 on 2020/12/08.
+//
+
+import UIKit
+
+
+final class ListCollectionViewDataSource: NSObject, UICollectionViewDataSource {
+  
+  // MARK: - Property
+  
+  private let viewModel: ListViewModelProtocol
+  
+  
+  // MARK: - Initializer
+  
+  init(viewModel: ListViewModelProtocol) {
+    self.viewModel = viewModel
+    
+    super.init()
+  }
+  
+  
+  // MARK: - Method
+  
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return viewModel.numberOfCards()
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell: ListCollectionViewCell = collectionView.dequeueReusableCell(forIndexPath: indexPath)
+    
+    cell.update(with: viewModel.fetchCard(at: indexPath.row))
+    
+    return cell
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+    switch kind {
+    case UICollectionView.elementKindSectionHeader:
+      let headerView: ListHeaderView = collectionView.dequeReusableHeaderView(forIndexPath: indexPath)
+      
+      headerView.update(with: viewModel)
+      
+      return headerView
+      
+    case UICollectionView.elementKindSectionFooter:
+      let footerView: ListFooterView = collectionView.dequeReusableFooterView(forIndexPath: indexPath)
+      return footerView
+      
+    default:
+      return UICollectionReusableView()
+    }
+  }
+}
