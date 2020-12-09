3786509d0f2de1f857087e43ca6a70effa6f05d1
youngxpepp <youngxpepp@gmail.com>
2020-12-09 17:12:03 +0900

    [FEAT] CommentService.addComment() 추가
    - 서비스 레이어에 댓글 추가 메서드 추가

--- server/src/service/CommentService.js
+++ server/src/service/CommentService.js
@@ -1,3 +1,7 @@
+import { Transactional } from 'typeorm-transactional-cls-hooked';
+import { EntityNotFoundError } from '../common/error/EntityNotFoundError';
+import { ForbiddenError } from '../common/error/ForbiddenError';
+import { CommentDto } from '../dto/CommentDto';
 import { BaseService } from './BaseService';
 
 export class CommentService extends BaseService {
@@ -9,5 +13,39 @@ export class CommentService extends BaseService {
         }
 
         return CommentService.instance;
+    }
+
+    @Transactional()
+    async addComment({ userId, cardId, content }) {
+        const card = await this.customCardRepository.findWithListAndBoardById(cardId);
+
+        if (card === undefined) {
+            throw new EntityNotFoundError('Not found card');
+        }
+
+        const { list } = card;
+        const { board } = list;
+
+        const isAuthorized = await this.customBoardRepository.existUserByBoardId({
+            boardId: board.id,
+            userId,
+        });
+
+        if (!isAuthorized) {
+            throw new ForbiddenError(`You're not authorized`);
+        }
+
+        const comment = this.commentRepository.create({
+            content,
+            user: {
+                id: userId,
+            },
+            card: {
+                id: card.id,
+            },
+        });
+        await this.commentRepository.save(comment);
+
+        return comment;
     }
 }
---
+++ server/test/service/comment/CommentService.addComment.test.js
@@ -0,0 +1,177 @@
+import moment from 'moment';
+import { getEntityManagerOrTransactionManager } from 'typeorm-transactional-cls-hooked';
+import { Application } from '../../../src/Application';
+import { EntityNotFoundError } from '../../../src/common/error/EntityNotFoundError';
+import { ForbiddenError } from '../../../src/common/error/ForbiddenError';
+import { Board } from '../../../src/model/Board';
+import { Card } from '../../../src/model/Card';
+import { Invitation } from '../../../src/model/Invitation';
+import { List } from '../../../src/model/List';
+import { User } from '../../../src/model/User';
+import { CommentService } from '../../../src/service/CommentService';
+import { TestTransactionDelegate } from '../../TestTransactionDelegate';
+
+describe('CommentService.addComment() Test', () => {
+    const app = new Application();
+
+    beforeAll(async () => {
+        await app.initEnvironment();
+        await app.initDatabase();
+    });
+
+    afterAll(async (done) => {
+        await app.close();
+        done();
+    });
+
+    test('없는 카드에 대해 댓글을 작성할 때 EntityNotFoundError 발생', async () => {
+        const commentService = CommentService.getInstance();
+        await TestTransactionDelegate.transaction(async () => {
+            // given
+            const em = getEntityManagerOrTransactionManager('default');
+
+            const user0 = em.create(User, {
+                socialId: 0,
+                name: 'youngxpepp',
+                profileImageUrl: 'http://',
+            });
+            await em.save(user0);
+
+            // when
+            // then
+            try {
+                await commentService.addComment({
+                    userId: user0.id,
+                    cardId: 1,
+                    content: 'whats up',
+                });
+                fail();
+            } catch (error) {
+                expect(error).toBeInstanceOf(EntityNotFoundError);
+                expect(error.message).toEqual('Not found card');
+            }
+        });
+    });
+
+    test('user0이 카드에 댓글을 남기려 하지만 보드에 초대되지 않아서 ForBiddenError 발생', async () => {
+        const commentService = CommentService.getInstance();
+        await TestTransactionDelegate.transaction(async () => {
+            // given
+            const em = getEntityManagerOrTransactionManager('default');
+
+            const user0 = em.create(User, {
+                socialId: 0,
+                name: 'youngxpepp',
+                profileImageUrl: 'http://',
+            });
+            const user1 = em.create(User, {
+                socialId: 1,
+                name: 'sooyeon',
+                profileImageUrl: 'http://',
+            });
+            await em.save([user0, user1]);
+
+            const board0 = em.create(Board, {
+                title: 'board title 0',
+                color: '#FFFFFF',
+                creator: user1,
+            });
+            await em.save(board0);
+
+            const list0 = em.create(List, {
+                title: 'list title 0',
+                position: 0,
+                board: board0,
+                creator: user1,
+            });
+            await em.save(list0);
+
+            const card0 = em.create(Card, {
+                title: 'card title 0',
+                content: 'card content 0',
+                position: 0,
+                dueDate: moment.tz('2020-12-03T09:37:00', 'Asia/Seoul').format(),
+                list: list0,
+                creator: user1,
+            });
+            await em.save(card0);
+
+            // when
+            // then
+            try {
+                await commentService.addComment({
+                    userId: user0.id,
+                    cardId: card0.id,
+                    content: 'edited card content 0',
+                });
+                fail();
+            } catch (error) {
+                expect(error).toBeInstanceOf(ForbiddenError);
+                expect(error.message).toEqual(`You're not authorized`);
+            }
+        });
+    });
+
+    test('user0이 card0에 댓글을 남긴다', async () => {
+        const commentService = CommentService.getInstance();
+        await TestTransactionDelegate.transaction(async () => {
+            // given
+            const em = getEntityManagerOrTransactionManager('default');
+
+            const user0 = em.create(User, {
+                socialId: 0,
+                name: 'youngxpepp',
+                profileImageUrl: 'http://',
+            });
+            const user1 = em.create(User, {
+                socialId: 1,
+                name: 'sooyeon',
+                profileImageUrl: 'http://',
+            });
+            await em.save([user0, user1]);
+
+            const board0 = em.create(Board, {
+                title: 'board title 0',
+                color: '#FFFFFF',
+                creator: user1,
+            });
+            await em.save(board0);
+
+            const list0 = em.create(List, {
+                title: 'list title 0',
+                position: 0,
+                board: board0,
+                creator: user1,
+            });
+            await em.save(list0);
+
+            const card0 = em.create(Card, {
+                title: 'card title 0',
+                content: 'card content 0',
+                position: 0,
+                dueDate: moment.tz('2020-12-03T09:37:00', 'Asia/Seoul').format(),
+                list: list0,
+                creator: user1,
+            });
+            await em.save(card0);
+
+            await em.save(
+                em.create(Invitation, {
+                    user: user0,
+                    board: board0,
+                }),
+            );
+
+            // when
+            const comment = await commentService.addComment({
+                userId: user0.id,
+                cardId: card0.id,
+                content: 'edited card content 0',
+            });
+
+            // then
+            expect(comment.id).toEqual(expect.any(Number));
+            expect(comment.content).toEqual(`edited card content 0`);
+        });
+    });
+});
