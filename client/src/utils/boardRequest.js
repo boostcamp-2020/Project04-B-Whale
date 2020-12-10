6cb59cdd19de5a9b4ae0dcf50b0d86ce78aa2739
Sooyeon Park <49746644+Park-SooYeon@users.noreply.github.com>
2020-12-10 16:39:28 +0900

    마감기한 모달 컴포넌트 추가
    * [CHORE] datepicker 사용을 위한 npm 설치
    
    - datepicker 사용을 위한 react-datepicker npm 설치
    - locale 설정을 위한 date-fns npm 설치
    
    * [FEAT] 마감기한 선택 모달 컴포넌트 추가
    
    * [FEAT] 마감기한 저장하기 버튼 클릭 이벤트 추가
    
    - API 요청을 위한 modifyCardDueDate 함수 추가
    - 마감기한 저장하기 버튼 클릭 이벤트 추가
    
    * [CHORE] header logo 이미지 css 수정
    
    * [FEAT] axios interceptor 추가
    
    - axios interceptor 추가
    - 응답 결과에 따라 에러 핸들링
    
    * [FIX] axios interceptor 추가에 따른 오류 수정
    
    - api 요청을 axios interceptor로 하도록 수정
    - interceptor를 사용함에 따라 발생한 오류 수정
    
    * [CHORE] datepicker css 수정

--- client/src/utils/boardRequest.js
+++ client/src/utils/boardRequest.js
@@ -1,4 +1,4 @@
-import request from './api';
+import request from './request';
 
 export const getBoards = async () => {
     const config = {
---
+++ client/src/utils/request.js
@@ -0,0 +1,58 @@
+/* eslint-disable no-alert */
+import axios from 'axios';
+
+const token = localStorage.getItem('jwt');
+
+const instance = axios.create({
+    baseURL: process.env.REACT_APP_BASE_URL,
+    timeout: 1000,
+    headers: {
+        Authorization: token,
+        'Content-Type': 'application/json',
+    },
+});
+
+instance.interceptors.request.use(
+    (config) => config,
+    (error) => Promise.reject(error),
+);
+
+instance.interceptors.response.use(
+    (response) => {
+        const { status } = response;
+        switch (status) {
+            case 200:
+            case 204:
+                break;
+            case 201:
+                alert('정상적으로 생성되었습니다.');
+                break;
+            default:
+                break;
+        }
+        return { data: response.data };
+    },
+
+    (error) => {
+        const { status } = error.response;
+        switch (status) {
+            case 400:
+            case 409:
+                alert('입력 값을 확인해주세요.');
+                break;
+            case 401:
+                window.location.href = '/login';
+                break;
+            case 403:
+                alert('권한이 없습니다.');
+                break;
+            case 404:
+                alert('알 수 없는 요청입니다');
+                break;
+            default:
+                alert('이유를 알 수 없는 오류입니다.');
+        }
+    },
+);
+
+export default instance;
