8dff0edb21be3d7cdadb2c49c81fff9f8bbc8e2a
tksrl0379 <tksrl0379@naver.com>
2020-12-10 11:03:42 +0900

    [REFACTOR] cell 에서 유저 정보를 나타내도록 변경
    - cell 에서 유저 정보를 나타내도록 변경

--- iOS/AreUDone/AreUDone/InvitationScene/View/InvitationTableViewCell.swift
+++ iOS/AreUDone/AreUDone/InvitationScene/View/InvitationTableViewCell.swift
@@ -9,6 +9,19 @@ import UIKit
 
 final class InvitationTableViewCell: UITableViewCell, Reusable {
 
+  private lazy var profileImageView: UIImageView = {
+    let imageView = UIImageView()
+    imageView.translatesAutoresizingMaskIntoConstraints = false
+    
+    imageView.layer.borderWidth = 0.5
+    imageView.layer.borderColor = UIColor.darkGray.cgColor
+    imageView.layer.masksToBounds = true
+
+    imageView.image = UIImage(systemName: "person")
+    
+    return imageView
+  }()
+  
   // MARK: - Initializer
   
   required init?(coder: NSCoder) {
@@ -22,6 +35,14 @@ final class InvitationTableViewCell: UITableViewCell, Reusable {
     
     configure()
   }
+  
+  
+  // MARK: - Method
+ 
+  func update(with data: Data, and user: User) {
+    profileImageView.image = UIImage(data: data)
+    textLabel?.text = user.name
+  }
 }
 
 
@@ -30,6 +51,26 @@ final class InvitationTableViewCell: UITableViewCell, Reusable {
 private extension InvitationTableViewCell {
   
   func configure() {
+    addSubview(profileImageView)
+    
+    configureView()
+    configureProfileImageView()
+  }
+  
+  func configureView() {
+    textLabel?.textAlignment = .center
     accessoryView = UIImageView(image: UIImage(systemName: "plus.circle"))
+  }
+  
+  func configureProfileImageView() {
+    NSLayoutConstraint.activate([
+      profileImageView.topAnchor.constraint(equalTo: topAnchor, constant: 5),
+      profileImageView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -5),
+      profileImageView.widthAnchor.constraint(equalTo: profileImageView.heightAnchor),
+      profileImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20)
+    ])
+    
+    profileImageView.layoutIfNeeded()
+    profileImageView.layer.cornerRadius = profileImageView.bounds.width/2
   }
 }
