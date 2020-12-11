dfeea9ab3bdab092bbbba7c86b043df0496c7ebc
dh00n <43809418+dh00n@users.noreply.github.com>
2020-12-12 00:28:36 +0900

    리스트 컴포넌트, 리스트추가 인풋박스, 카드추가 인풋박스 컴포넌트 / 각종 모달창 구현 완료
    * [CHORE] npm 앤트디자인 추가
    - datetime input을 위한 앤트디자인 추가
    
    * [FEAT] 리스트 및 카드 추가 시에 사용할 버튼, 인풋 컴포넌트 구현
    
    * [FEAT] 리스트 개별 컴포넌트 구현
    - 리스트 개별 컴포넌트 구현
    - 타이틀, 카드 들, 카드 추가 컴포넌트
    
    * [FEAT] 리스트-카드 컴포넌트 삽입 위한 Wrapper 생성
    - 전역컨텍스트의 리스트들을 뿌려주는 Wrapper 구현
    
    * [CHORE] antd 추가
    
    * [FEAT] 리스트 생성 구현
    - 리스트 생성 핸들러 구현
    
    * [STYLE] 드롭다운 높이값 추가
    
    * [STYLE] 드롭다운 높이값 추가,boardDetail context 의 리스트 사용
    
    * [FEAT] List 컴포넌트 구현
    
    * [FEAT] 리스트 내 메뉴컴포넌트 구현
    - 리스트 이동
    - 리스트 삭제
    
    * [FEAT] 머트리얼 UI 모달 컴포넌트 구현
    
    * [STYLE] 상단 메뉴바 드롭다운 높이값 추가
    
    * [FEAT] 리스트 요청 모듈 추가
    - 추가, 삭제, 수정 함수
    
    * [STYLE] css 수정
    - zindex 변경
    
    * [STYLE] css , styled 컴포넌트로 분리
    - antd 모달 등 추가
    
    * [FEAT] 리스트 컴포넌트 우측 상단 메뉴, 모달 구현
    
    * [FIX] 리스트, 카드 추가 타이틀 인풋 antd로 변경
    
    * [CHORE] 머테리얼 유아이 제거
    
    * [FIX] 날짜 변환 moment 사용
    - YYYY-MM-DD HH:mm:ss moment format 사용
    
    * [FIX] 리뷰 사항 반영
    - switch 일단 제거

--- client/src/components/common/CreateBoardModal.js
+++ client/src/components/common/CreateBoardModal.js
@@ -11,7 +11,7 @@ const DimmedModal = styled.div`
     bottom: 0;
     right: 0;
     background-color: rgba(0, 0, 0, 0.6);
-    z-index: 1;
+    z-index: 2;
 `;
 
 const ModalWrapper = styled.div`
@@ -21,7 +21,7 @@ const ModalWrapper = styled.div`
     right: 0;
     bottom: 0;
     left: 0;
-    z-index: 2;
+    z-index: 3;
     overflow: auto;
     outline: 0;
 `;
