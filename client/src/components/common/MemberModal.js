88e274c469ad3f1ceced09a39577070d89461f3e
Sooyeon Park <49746644+Park-SooYeon@users.noreply.github.com>
2020-12-12 18:26:15 +0900

    카드 멤버 초대 모달 컴포넌트 구현
    * [FEAT] 멤버 추가/삭제 모달 컴포넌트 추가
    
    * [FEAT] 멤버 추가/삭제 모달 클릭 이벤트 추가
    
    - 멤버를 클릭하면 추가 및 삭제를 하도록 클릭 이벤트 추가
    - 카드의 멤버로 속한 유저는 오른쪽에 체크표시가 추가됨
    
    * [FEAT] 멤버 추가/삭제 요청 함수 추가 및 요청 이벤트 추가
    
    - 멤버 추가/삭제 API를 호출할 addMemberToCard 함수 추가
    - 멤버 추가/삭제 모달 컴포넌트가 닫힐 때, 요청을 보내도록 클릭 이벤트 추가
    - request 성공 시, 모든 response 정보를 받도록 수정
    - 변수명 변경
    
    * [FEAT] 멤버 검색 기능 추가
    
    - 보드에 초대된 멤버를 이름으로 검색할 수 있는 기능 추가
    
    * [STYLE] 컴포넌트명 변경 및 모달 header 추가
    
    * [FIX] 스페이스바를 누르면 검색 안되는 오류 수정
    
    - 스페이스바를 누르면 현재 입력된 값에 포함되는 유저가 검색 안되는 오류 수정
    
    * [FEAT] 검색된 멤버 데이터 sort 기능 추가
    
    - 멤버로 선택된 유저가 멤버 상단에 오도록 data sort 기능 추가
    
    * [FIX] lint error 및 sort 오류 수정
    
    * [FIX] 유저 검색 시, \ 입력 오류 수정
    
    - \를 입력하면 정규식 오류나는 문제 수정

---
+++ client/src/components/common/MemberModal.js
@@ -0,0 +1,113 @@
+import React, { useContext, useState } from 'react';
+import styled from 'styled-components';
+import { IoIosClose } from 'react-icons/io';
+import BoardDetailContext from '../../context/BoardDetailContext';
+import { addMemberToCard } from '../../utils/cardRequest';
+import Member from './Member';
+
+const Wrapper = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+`;
+
+const ModalWrapper = styled.div`
+    position: absolute;
+    top: 2.2em;
+    left: 0.7em;
+    z-index: 2;
+    max-height: 500px;
+    background-color: ${(props) => props.theme.whiteColor};
+    border: ${(props) => props.theme.border};
+    border-radius: ${(props) => props.theme.radiusSmall};
+    overflow: scroll;
+`;
+
+const ModalHeader = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 5px 3px;
+    border-bottom: ${(props) => props.theme.border};
+`;
+
+const ModalTitle = styled.div`
+    font-size: 18px;
+    font-weight: 700;
+`;
+
+const SearchInput = styled.input.attrs({
+    type: 'text',
+    placeholder: '이름으로 검색',
+})`
+    height: 35px;
+    margin: 5px 5px;
+    padding: 0 3px;
+    border: 3px solid lightgray;
+
+    &:focus {
+        outline: 1px solid blue;
+    }
+`;
+
+const MemberModal = ({ onClose }) => {
+    const { boardDetail } = useContext(BoardDetailContext);
+    const { creator, invitedUsers } = boardDetail;
+    // TODO: 카드에 members 정보를 default 값으로 변경
+    const [userIds, setUserIds] = useState([1, 2]);
+    const allInvitedUsers = [creator, ...invitedUsers];
+    const [searchedUsers, setSearchedUsers] = useState(allInvitedUsers);
+    allInvitedUsers.sort((a) => (userIds.includes(a.id) ? -1 : 1));
+
+    const onClickClose = async (e) => {
+        if (e.target === e.currentTarget) {
+            // TODO: context에 저장된 카드 id 가져오도록 변경할 것
+            const cardId = 1;
+            await addMemberToCard({ cardId, userIds });
+            onClose();
+        }
+    };
+
+    const onChangeUserSearch = (e) => {
+        const text = e.target.value;
+        if (text.includes('\\')) return;
+        if (text === '') {
+            setSearchedUsers(allInvitedUsers);
+            return;
+        }
+
+        const regex = new RegExp(`^${text}`);
+        const newSearchUsers = allInvitedUsers.filter((user) => user.name.search(regex) !== -1);
+        newSearchUsers.sort((a) => (userIds.includes(a.id) ? -1 : 1));
+        setSearchedUsers(newSearchUsers);
+    };
+
+    return (
+        <>
+            <Wrapper onClick={onClickClose} />
+            <ModalWrapper>
+                <ModalHeader>
+                    <div />
+                    <ModalTitle>멤버</ModalTitle>
+                    <IoIosClose size={24} cursor="pointer" onClick={onClose} />
+                </ModalHeader>
+                <SearchInput onChange={onChangeUserSearch} />
+                {searchedUsers.map(({ id, profileImageUrl, name }) => (
+                    <Member
+                        key={id}
+                        memberId={id}
+                        profileImageUrl={profileImageUrl}
+                        name={name}
+                        checked={userIds.includes(id)}
+                        selectedMember={userIds}
+                        changeMember={setUserIds}
+                    />
+                ))}
+            </ModalWrapper>
+        </>
+    );
+};
+
+export default MemberModal;
--- client/src/utils/cardRequest.js
+++ client/src/utils/cardRequest.js
@@ -35,6 +35,17 @@ export const modifyCardDueDate = async ({ cardId, dueDate }) => {
     return response;
 };
 
+export const addMemberToCard = async ({ cardId, userIds }) => {
+    const config = {
+        url: `/api/card/${cardId}/member`,
+        method: 'PUT',
+        data: { userIds },
+    };
+    const response = await request(config);
+
+    return response;
+};
+      
 export const modifyCardPosition = async ({ cardId, listId, position }) => {
     const config = {
         url: `/api/card/${cardId}`,
