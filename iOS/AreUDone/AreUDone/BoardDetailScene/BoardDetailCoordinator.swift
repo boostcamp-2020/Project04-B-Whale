f6b1feb713a0ff2656b49bb8a2a62d8e74f56118
tksrl0379 <tksrl0379@naver.com>
2020-12-13 23:41:41 +0900

    [FEAT] ListService 에 LocalDataSource 객체 주입
    - ListService 에 LocalDataSource 객체 주입

--- iOS/AreUDone/AreUDone/BoardDetailScene/BoardDetailCoordinator.swift
+++ iOS/AreUDone/AreUDone/BoardDetailScene/BoardDetailCoordinator.swift
@@ -41,7 +41,7 @@ final class BoardDetailCoordinator: NavigationCoordinator {
               guard let self = self else { return UIViewController()}
               
               let boardService = BoardService(router: self.router, localDataSource: BoardLocalDataSource())
-              let listService = ListService(router: self.router)
+              let listService = ListService(router: self.router, localDataSource: ListLocalDataSource())
               let cardService = CardService(router: self.router)
               let activityService = ActivityService(router: MockRouter(jsonFactory: ActivityTrueJsonFactory()))
               let imageService = ImageService(router: self.router)
