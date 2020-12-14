2b7d111ec673035b7ee210c67d4b379ceec92f43
Seo Myung Ryul <smr3102@naver.com>
2020-12-14 17:22:42 +0900

    [REFACTOR] CardDetail 모델 변경
    - realm 에 저장할 수 있는 객체로 변경

--- iOS/AreUDone/AreUDone/Common/Model/CardDetail.swift
+++ iOS/AreUDone/AreUDone/Common/Model/CardDetail.swift
@@ -6,56 +6,152 @@
 //
 
 import Foundation
+import RealmSwift
 
-struct CardDetail: Codable {
+class CardDetail: Object, Codable {
   
-  let id: Int
-  let title: String
-  let content: String?
-  let dueDate: String
-  let members: [User]?
-  let comments: [Comment]?
-  let board: Board
-  let list: List
+  @objc dynamic var id: Int = 0
+  @objc dynamic var title: String = ""
+  @objc dynamic var content: String = ""
+  @objc dynamic var dueDate: String = ""
+  var members = List<User>()
+  var comments = List<CardDetailComment>()
+  @objc dynamic var board: CardDetailBoard?
+  @objc dynamic var list: CardDetailList?
   
-  struct Comment: Codable {
+  required convenience init(from decoder: Decoder) throws {
+    self.init()
+    let container = try decoder.container(keyedBy: CodingKeys.self)
     
-    let id: Int
-    let content: String
-    let createdAt: String
-    let user: User
+    self.id = try container.decode(Int.self, forKey: .id)
+    self.title = try container.decode(String.self, forKey: .title)
+    self.content = try container.decode(String.self, forKey: .content)
+    self.dueDate = try container.decode(String.self, forKey: .dueDate)
+    self.board = try container.decode(CardDetailBoard.self, forKey: .board)
+    self.list = try container.decode(CardDetailList.self, forKey: .list)
+    let decodedMembers = try container.decodeIfPresent([User].self, forKey: .members) ?? [User]()
+    let decodedComments = try container.decodeIfPresent([CardDetailComment].self, forKey: .comments) ?? [CardDetailComment]()
     
-    struct User: Codable, Hashable {
-      let id: Int
-      let name: String
-      let profileImageUrl: String
+    self.members.append(objectsIn: decodedMembers)
+    self.comments.append(objectsIn: decodedComments)
+  }
+  
+  func fetchMembers() -> [User] {
+    var fetchedUsers = [User]()
+    fetchedUsers.append(contentsOf: members)
+    
+    return fetchedUsers
+  }
+  
+  func fetchComment() -> [CardDetailComment] {
+    var fetchedComments = [CardDetailComment]()
+    fetchedComments.append(contentsOf: comments)
+    
+    return fetchedComments
+  }
+  
+  override class func primaryKey() -> String? {
+    return "id"
+  }
+}
+
+class CardDetailComment: Object, Codable {
+  
+  @objc dynamic var id: Int = 0
+  @objc dynamic var content: String = ""
+  @objc dynamic var createdAt: String = ""
+  @objc dynamic var user: User?
+  
+  required convenience init(from decoder: Decoder) throws {
+    self.init()
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    
+    self.id = try container.decode(Int.self, forKey: .id)
+    self.content = try container.decode(String.self, forKey: .content)
+    self.createdAt = try container.decode(String.self, forKey: .createdAt)
+    self.user = try container.decode(User.self, forKey: .user)
+  }
+  
+  override func isEqual(_ object: Any?) -> Bool {
+    guard let other = object as? CardDetailComment else { return false }
+    
+    return self.id == other.id
+  }
+  
+  override class func primaryKey() -> String? {
+    return "id"
+  }
+}
+
+class CardDetailBoard: Object, Codable {
+  @objc dynamic var id: Int = 0
+  @objc dynamic var title: String = ""
+  
+  required convenience init(from decoder: Decoder) throws {
+    self.init()
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    
+    self.id = try container.decode(Int.self, forKey: .id)
+    self.title = try container.decode(String.self, forKey: .title)
+  }
+}
+
+
+class CardDetailList: Object, Codable {
+  @objc dynamic var id: Int = 0
+  @objc dynamic var title: String = ""
+  
+    required convenience init(from decoder: Decoder) throws {
+      self.init()
+      let container = try decoder.container(keyedBy: CodingKeys.self)
+
+      self.id = try container.decode(Int.self, forKey: .id)
+      self.title = try container.decode(String.self, forKey: .title)
     }
-  }
-  
-  struct Board: Codable {
-    let id: Int
-    let title: String
-  }
-  
-  struct List: Codable {
-    let id: Int
-    let title: String
-  }
 }
-
-extension CardDetail.Comment: Hashable {
-  
-  static func == (lhs: CardDetail.Comment, rhs: CardDetail.Comment) -> Bool {
-    return lhs.id == rhs.id &&
-      lhs.content == rhs.content &&
-      lhs.createdAt == rhs.createdAt &&
-      lhs.user == rhs.user
-  }
-  
-  func hash(into hasher: inout Hasher) {
-    hasher.combine(id)
-    hasher.combine(content)
-    hasher.combine(createdAt)
-    hasher.combine(user)
-  }
-}
+//struct CardDetail: Codable {
+//
+//  let id: Int
+//  let title: String
+//  let content: String?
+//  let dueDate: String
+//  let members: [User]?
+//  let comments: [Comment]?
+//  let board: Board
+//  let list: List
+//
+//  struct Comment: Codable {
+//
+//    let id: Int
+//    let content: String
+//    let createdAt: String
+//    let user: User
+//  }
+//
+//  struct Board: Codable {
+//    let id: Int
+//    let title: String
+//  }
+//
+//  struct List: Codable {
+//    let id: Int
+//    let title: String
+//  }
+//}
+//
+//extension CardDetail.Comment: Hashable {
+//
+//  static func == (lhs: CardDetail.Comment, rhs: CardDetail.Comment) -> Bool {
+//    return lhs.id == rhs.id &&
+//      lhs.content == rhs.content &&
+//      lhs.createdAt == rhs.createdAt &&
+//      lhs.user == rhs.user
+//  }
+//
+//  func hash(into hasher: inout Hasher) {
+//    hasher.combine(id)
+//    hasher.combine(content)
+//    hasher.combine(createdAt)
+//    hasher.combine(user)
+//  }
+//}
