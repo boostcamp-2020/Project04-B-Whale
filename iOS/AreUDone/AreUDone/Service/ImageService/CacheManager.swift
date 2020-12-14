b482c7c6259e3d9edcdeadbac33ba14f9a5abd20
tksrl0379 <tksrl0379@naver.com>
2020-12-15 02:21:05 +0900

    [FEAT] CacheManager 구현 및 ImageService 에 적용
    - CacheManager 구현 및 ImageService 에 적용

--- iOS/AreUDone/AreUDone/Service/ImageService/CacheManager.swift
+++ iOS/AreUDone/AreUDone/Service/ImageService/CacheManager.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+import NetworkFramework
+
+protocol CacheManageable {
+  
+  func load(imageName: String) -> Data?
+}
+
+struct CacheManager: CacheManageable {
+  
+  // MARK: - Method
+  
+  func load(imageName: String) -> Data? {
+    guard
+      let imagePath = urlOfCached(imageName: imageName),
+      let data = try? Data(contentsOf: imagePath)
+    else {
+      return nil
+    }
+    return data
+  }
+  
+  private func urlOfCached(imageName: String) -> URL? {
+    guard let cachedPath = Path.cached else { return nil }
+    let imagePath = cachedPath.appendingPathComponent(imageName)
+    
+    if FileManager.default.fileExists(atPath: imagePath.path) {
+      return imagePath
+    }
+    return nil
+  }
+}
