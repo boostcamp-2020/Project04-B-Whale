83f9323a61129c93d950e28b366375a9a3b29f7c
donghoon <0930dhs@gmail.com>
2020-12-16 18:45:15 +0900

    [TEST] 보드 삭제 api 테스트코드 작성 - 보드삭제 api 라우터 테스트 구현 - 보드 삭제 서비스함수 테스트 구현

---
+++ server/test/router/board/BoardRouter.deleteBoard.test.js
@@ -0,0 +1,76 @@
+import request from 'supertest';
+import { getRepository } from 'typeorm';
+import { Application } from '../../../src/Application';
+import { JwtUtil } from '../../../src/common/util/JwtUtil';
+import { Board } from '../../../src/model/Board';
+import { User } from '../../../src/model/User';
+import { TransactionRollbackExecutor } from '../../TransactionRollbackExecutor';
+
+describe('DELETE /api/card/:cardId', () => {
+    let app = null;
+    let jwtUtil = null;
+
+    beforeAll(async () => {
+        app = new Application();
+        await app.initialize();
+        jwtUtil = JwtUtil.getInstance();
+    });
+
+    afterAll(async (done) => {
+        await app.close();
+        done();
+    });
+
+    test('DELETE /api/board/:id 보드 삭제api 호출할 때, 보드 미존재시 404을 리턴한다.', async () => {
+        await TransactionRollbackExecutor.rollback(async () => {
+            // given
+            const user = { name: 'user', socialId: '1234', profileImageUrl: 'image' };
+            const userRepository = getRepository(User);
+            const createUser = userRepository.create(user);
+            await userRepository.save(createUser);
+            const token = await jwtUtil.generateAccessToken({
+                userId: createUser.id,
+                username: createUser.name,
+            });
+            const boardId = 0;
+
+            // when
+            const response = await request(app.httpServer).delete(`/api/board/${boardId}`).set({
+                Authorization: token,
+                'Content-Type': 'application/json',
+            });
+
+            // then
+            expect(response.status).toEqual(404);
+        });
+    });
+
+    test('DELETE /api/board/:id 보드 삭제api 호출할 때, 정상 호출 시 204을 리턴한다.', async () => {
+        await TransactionRollbackExecutor.rollback(async () => {
+            // given
+            const user = { name: 'dh', socialId: '1234', profileImageUrl: 'image' };
+            const userRepository = getRepository(User);
+            const createUser = userRepository.create(user);
+            await userRepository.save(createUser);
+            const token = await jwtUtil.generateAccessToken({
+                userId: createUser.id,
+                username: createUser.name,
+            });
+
+            const boardRepository = getRepository(Board);
+            const board = { title: 'board', creator: createUser.id, color: '#ffffff' };
+            const createBoard = boardRepository.create(board);
+            await boardRepository.save(createBoard);
+
+            // when
+            const response = await request(app.httpServer)
+                .delete(`/api/board/${createBoard.id}`)
+                .set({
+                    Authorization: token,
+                    'Content-Type': 'application/json',
+                });
+
+            expect(response.status).toEqual(204);
+        });
+    });
+});
