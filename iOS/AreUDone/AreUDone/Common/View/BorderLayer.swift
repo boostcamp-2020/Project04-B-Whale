34fa587305f42774551cf43e2e8f578588b55818
tksrl0379 <tksrl0379@naver.com>
2020-12-16 23:01:00 +0900

    [FEAT] 화면 전체 테두리를 담당할 커스텀 뷰 객체 생성
    - 화면 전체 테두리를 담당할 커스텀 뷰 객체 생성

---
+++ iOS/AreUDone/AreUDone/Common/View/BorderLayer.swift
@@ -0,0 +1,40 @@
+//
+//  BorderLayer.swift
+//  AreUDone
+//
+//  Created by a1111 on 2020/12/16.
+//
+
+import UIKit
+
+final class BorderLayer: CALayer {
+  
+  // MARK: - Initializer
+  
+  override init() {
+    super.init()
+  }
+  
+  convenience init(frame: CGRect) {
+    self.init()
+    
+    self.frame = frame
+    configure()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("This class should be initialized with code")
+  }
+}
+
+
+// MARK: - Extension Configure Method
+
+extension BorderLayer {
+  
+  func configure() {
+    backgroundColor = UIColor.clear.cgColor
+    cornerRadius = frame.height / 22
+  }
+}
+
