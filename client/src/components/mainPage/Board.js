ef105116b7c05e9d302264efe18df0c796e51378
수연 <cjg03194@gmail.com>
2020-12-17 20:27:22 +0900

    [STYLE] 보드 제목이 길 경우 생략하도록 style 수정

--- client/src/components/mainPage/Board.js
+++ client/src/components/mainPage/Board.js
@@ -3,13 +3,15 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const BoardTitle = styled.li`
-    padding: 8px 0;
+    padding: 8px;
     margin-bottom: 2px;
     border: ${(props) => props.theme.border};
     font-size: 18px;
     font-weight: 700;
     text-align: center;
     cursor: pointer;
+    text-overflow: ellipsis;
+    overflow-x: hidden;
 `;
 
 const Board = ({ id, title }) => {
