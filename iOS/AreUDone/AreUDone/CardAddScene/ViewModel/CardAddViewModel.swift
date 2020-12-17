7956d1748be515045d6eada639f7ff58c0cf5cba
Seo Myung Ryul <smr3102@naver.com>
2020-12-17 14:21:53 +0900

    [FIX] 변수명 오타 수정
    - CardAddViewModel에서 listTitle -> cardTitle

--- iOS/AreUDone/AreUDone/CardAddScene/ViewModel/CardAddViewModel.swift
+++ iOS/AreUDone/AreUDone/CardAddScene/ViewModel/CardAddViewModel.swift
@@ -45,9 +45,9 @@ final class CardAddViewModel: CardAddViewModelProtocol {
   private var pushContentInputHandler: ((String) -> Void)?
   private var updateTableViewHandler: (() -> Void)?
   
-  private var listTitle: String = "" {
+  private var cardTitle: String = "" {
     didSet {
-      check(title: listTitle)
+      check(title: cardTitle)
     }
   }
   private var selectedDate: Date = Date()
@@ -66,7 +66,7 @@ final class CardAddViewModel: CardAddViewModelProtocol {
   
   func updateListTitle(to title: String) {
     let trimmedTitle = title.trimmingCharacters(in: .whitespacesAndNewlines)
-    listTitle = trimmedTitle
+    cardTitle = trimmedTitle
   }
   
   func presentCalendar() {
@@ -101,7 +101,7 @@ final class CardAddViewModel: CardAddViewModelProtocol {
     let listId = viewModel.fetchListId()
     cardService.createCard(
       listId: listId,
-      title: listTitle,
+      title: cardTitle,
       dueDate: dateString,
       content: content
     ) { result in
