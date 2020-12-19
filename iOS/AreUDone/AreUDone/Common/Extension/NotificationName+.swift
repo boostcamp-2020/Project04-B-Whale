accea630b57b46dafd99e16ac32732ed66f27e81
tksrl0379 <tksrl0379@naver.com>
2020-12-19 17:55:30 +0900

    [REFACTOR] 문자열 상수화
    - 문자열 상수화

--- iOS/AreUDone/AreUDone/Common/Extension/NotificationName+.swift
+++ iOS/AreUDone/AreUDone/Common/Extension/NotificationName+.swift
@@ -14,4 +14,6 @@ extension Notification.Name {
   static let networkChanged = Notification.Name("networkChanged")
   static let cardWillDragged = Notification.Name("CardWillDragged")
   static let cardDidDragged = Notification.Name("CardDidDragged")
+  static let foreground = Notification.Name("foreground")
+  static let background = Notification.Name("background")
 }
