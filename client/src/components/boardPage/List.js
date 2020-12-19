525bf60c7732bb5bf24ac18d11040c18de0acf81
donghoon <0930dhs@gmail.com>
2020-12-19 21:08:49 +0900

    [FIX] 카드 모달 내 리스트 아이디 추가

--- client/src/components/boardPage/List.js
+++ client/src/components/boardPage/List.js
@@ -290,6 +290,7 @@ export default function List({ title, id, index, moveList, position }) {
                         setCardModalVisible(false);
                     }}
                     cardId={cardId}
+                    listId={id}
                 />
             )}
         </>
