16f56188a96ada3b970bbaf5c1b647bce1492e3d
Sooyeon Park <49746644+Park-SooYeon@users.noreply.github.com>
2020-12-20 00:40:35 +0900

    dueDate변경시, 달력 상태 변경 오류 및 카드 이동 모달 컴포넌트의 위치 오류 수정
    * [FIX] 카드 dueDate 변경시, 달력 상태가 변경되지 않는 오류 수정
    
    * [FIX] 카드 dueDate변경 시, 화면 깜빡이는 문제 수정
    
    - calendarStatus 변경에 따라, 모달도 재랜더링 되어 화면이 깜빡임
    
    * [FIX] 빈 리스트에서 새로 생성한 카드의 위치가 없는 오류 수정
    
    - 빈 리스트에 새로 카드가 생성되면 드래그앤드랍을 위해 둔 id가 0인 카드 제거
    
    * [FIX] 내카드만 보기 체크박스 해제해도 내카드만 보이는 오류 수정

--- client/src/context/CalendarContext.js
+++ client/src/context/CalendarContext.js
@@ -40,6 +40,13 @@ export const CalendarReducer = (state, action) => {
                 cardCount,
             };
         }
+        case 'CHANGE_DUEDATE': {
+            const { cardCount } = action;
+            return {
+                ...state,
+                cardCount,
+            };
+        }
         default:
             throw new Error(`Unhandled action type: ${action.type}`);
     }
