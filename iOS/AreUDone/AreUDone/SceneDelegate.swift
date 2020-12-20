f6d01be6835be313d5f7d55b99450e37e90dea7a
fElix <41328839+fElix-MR@users.noreply.github.com>
2020-12-20 20:33:20 +0900

    앱 마무리 작업
    * [FIX] 다크모드 사용하지 않도록 설정
    
    - 다크모드 사용하지 않도록 설정
    
    * [FIX] 가로 모드 방지
    
    - 가로 모드 방지
    
    * [FIX] SegmentedController 지시바 수정
    
    - 지시바가 사라지는 이슈 해결
    
    * [CHORE] background 영상 위치 이동
    
    - signin scene / resources로 이동

--- iOS/AreUDone/AreUDone/SceneDelegate.swift
+++ iOS/AreUDone/AreUDone/SceneDelegate.swift
@@ -40,6 +40,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
     guard let windowScene = (scene as? UIWindowScene) else { return }
     window = UIWindow(windowScene: windowScene)
+    window?.overrideUserInterfaceStyle = .light
+    
     let router = Router()
         
     configureSceneCoordinator(
